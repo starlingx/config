4477fcbb93511d5fc87fcde0e106f48e2ac21805
Al Bailey <Al.Bailey@windriver.com>
2018-04-30 13:23:17 -0500

    Sysinv tox updates. Prepare for bandit reports and test reports
    The old sysinv py27 target did not use testrepository so its results
    were not reported in jenkins. Now it will.
    The bandit target will now output to an xml file which can be parsed and
    reported in jenkins.
    
    Note:  running tox for sysinv will reports warnings about missing
    fmClientUI and missing ceph. this is acceptable as neither of those are
    installed in the tox environment, and are side effects of using fm-api
    
    Change-Id: I48c74e9a62cd6fd32ceb71e1994c55fa7e8c42cf

--- sysinv/sysinv/sysinv/sysinv/sanity_coverage.py
+++ sysinv/sysinv/sysinv/sysinv/sanity_coverage.py
@@ -1,21 +1,7 @@
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
-# implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 #
 # Copyright (c) 2018 Wind River Systems, Inc.
 #
-# The right to copy, distribute, modify, or otherwise make use
-# of this software may be licensed only pursuant to the terms
-# of an applicable Wind River license agreement.
+# SPDX-License-Identifier: Apache-2.0
 #
 
 from coverage import Coverage
