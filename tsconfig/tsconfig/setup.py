38a86ab5e4a93083d53b83e3483c3d0a75341700
Don Penney <don.penney@windriver.com>
2018-08-08 19:36:58 -0500

    Resolve bashate and pep8 warnings
    This update addresses existing bashate and pep8 warnings in
    cgcs-patch, patch-alarm, and tsconfig. The bulk of these updates
    are style and spacing changes, such as whitespace style conventions.
    
    Story: 2003371
    Task: 24433
    Depends-On: I03beab02aee1f47d5e66570c74a2eadf75e52da0
    Change-Id: I44b26d24788907bac0730a952d70ed4bafb87d90
    Signed-off-by: Don Penney <don.penney@windriver.com>

--- tsconfig/tsconfig/setup.py
+++ tsconfig/tsconfig/setup.py
@@ -9,11 +9,11 @@ SPDX-License-Identifier: Apache-2.0
 
 import setuptools
 
-setuptools.setup(name='tsconfig',
-      version='1.0.0',
-      description='tsconfig',
-      license='Apache-2.0',
-      packages=['tsconfig'],
-      entry_points={
-      }
+setuptools.setup(
+    name='tsconfig',
+    version='1.0.0',
+    description='tsconfig',
+    license='Apache-2.0',
+    packages=['tsconfig'],
+    entry_points={}
 )
