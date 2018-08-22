openstack-archive/stx-config

Fix alarm that appears for computes after enabling swift
- after enabling Swift, Config out-of-date alarm appears for
computes with remote nova-local when it should only appear for
controllers.
- added a new function, "_apply_nova_specific_changes" that
always gets called when we are creating/modifying an internal
Ceph backend. If 'nova' is a service affected, then we raise a
Config out-of-date alarm for the computes with remote
nova-local configuration.

Story: 2003411
Task: 24552

Change-Id: I6139d572e331f1442071fad3111665d4a99fce6d
Signed-off-by: Irina Mihai <irina.mihai@windriver.com>

sysinv/sysinv/sysinv/sysinv/api/controllers/v1/storage_ceph.py
@@ -602,6 +602,24 @@ def _apply_backend_changes(op, sb_obj):
         pass
 
 
+def _apply_nova_specific_changes(sb_obj, old_sb_obj=None):
+    """If the backend's services have been modified and nova has been either
+       added or (re)moved, set the hosts with compute functionality and a
+       certain nova-local instance backing to Config out-of-date.
+    """
+    services = api_helper.getListFromServices(sb_obj.as_dict())
+
+    if old_sb_obj:
+        old_services = api_helper.getListFromServices(old_sb_obj.as_dict())
+    else:
+        old_services = []
+    diff_services = set(services) ^ set(old_services)
+
+    if constants.SB_SVC_NOVA in diff_services:
+        pecan.request.rpcapi.set_remote_storage_computes_out_of_date(
+            pecan.request.context,
+            constants.LVG_NOVA_BACKING_REMOTE)
+
 #
 # Create
 #
@@ -688,6 +706,9 @@ def _create(storage_ceph):
 
     # Enable the backend:
     _apply_backend_changes(constants.SB_API_OP_CREATE, storage_backend_obj)
+
+    # Make any needed changes for nova local.
+    _apply_nova_specific_changes(storage_backend_obj)
 
     return storage_ceph_obj
 
@@ -1113,6 +1134,8 @@ def _patch(storceph_uuid, patch):
             # Enable the backend changes:
             _apply_backend_changes(constants.SB_API_OP_MODIFY,
                                    rpc_storceph)
+
+        _apply_nova_specific_changes(rpc_storceph, ostorceph)
 
         return StorageCeph.convert_with_links(rpc_storceph)
 

sysinv/sysinv/sysinv/sysinv/conductor/rpcapi.py
@@ -830,6 +830,17 @@ class ConductorAPI(sysinv.openstack.common.rpc.proxy.RpcProxy):
                                        sb_uuid=sb_uuid,
                                        services=services))
 
+    def set_remote_storage_computes_out_of_date(self, context, instance_backing):
+        """Synchronously, have the conductor set the hosts with compute
+           functionality and with a certain nova-local instance backing to
+           config out-of-date.
+           :param context: request context
+           :param instance_backing: the host's instance backing
+        """
+        return self.call(context,
+                         self.make_msg('set_remote_storage_computes_out_of_date',
+                                       instance_backing=instance_backing))
+
     def update_external_cinder_config(self, context):
         """Synchronously, have the conductor update Cinder Exernal(shared)
            on a controller.

starlingx/config

sysinv/sysinv/sysinv/sysinv/api/controllers/v1/storage_ceph.py
@@ -602,6 +602,24 @@ def _apply_backend_changes(op, sb_obj):
         pass
 
 
+def _apply_nova_specific_changes(sb_obj, old_sb_obj=None):
+    """If the backend's services have been modified and nova has been either
+       added or (re)moved, set the hosts with compute functionality and a
+       certain nova-local instance backing to Config out-of-date.
+    """
+    services = api_helper.getListFromServices(sb_obj.as_dict())
+
+    if old_sb_obj:
+        old_services = api_helper.getListFromServices(old_sb_obj.as_dict())
+    else:
+        old_services = []
+    diff_services = set(services) ^ set(old_services)
+
+    if constants.SB_SVC_NOVA in diff_services:
+        pecan.request.rpcapi.config_update_nova_local_backed_hosts(
+            pecan.request.context,
+            constants.LVG_NOVA_BACKING_REMOTE)
+
 #
 # Create
 #
@@ -688,6 +706,9 @@ def _create(storage_ceph):
 
     # Enable the backend:
     _apply_backend_changes(constants.SB_API_OP_CREATE, storage_backend_obj)
+
+    # Make any needed changes for nova local.
+    _apply_nova_specific_changes(storage_backend_obj)
 
     return storage_ceph_obj
 
@@ -1113,6 +1134,8 @@ def _patch(storceph_uuid, patch):
             # Enable the backend changes:
             _apply_backend_changes(constants.SB_API_OP_MODIFY,
                                    rpc_storceph)
+
+        _apply_nova_specific_changes(rpc_storceph, ostorceph)
 
         return StorageCeph.convert_with_links(rpc_storceph)
 

sysinv/sysinv/sysinv/sysinv/conductor/rpcapi.py
@@ -404,6 +404,7 @@ class ConductorAPI(sysinv.openstack.common.rpc.proxy.RpcProxy):
     def update_partition_config(self, context, partition):
         """Asynchronously, have a conductor configure the physical volume
         partitions.
+
         :param context: request context.
         :param partition: dict with partition details.
         """
@@ -829,6 +830,18 @@ class ConductorAPI(sysinv.openstack.common.rpc.proxy.RpcProxy):
                          self.make_msg('update_ceph_external_config',
                                        sb_uuid=sb_uuid,
                                        services=services))
+
+    def config_update_nova_local_backed_hosts(self, context, instance_backing):
+        """Synchronously, have the conductor set the hosts with compute
+           functionality and with a certain nova-local instance backing to
+           config out-of-date.
+
+           :param context: request context
+           :param instance_backing: the host's instance backing
+        """
+        return self.call(context,
+                         self.make_msg('config_update_nova_local_backed_hosts',
+                                       instance_backing=instance_backing))
 
     def update_external_cinder_config(self, context):
         """Synchronously, have the conductor update Cinder Exernal(shared)