201f4b154013c9b61243e52b5c87ba73a2389d58
Dean Troyer <dtroyer@gmail.com>
2018-08-23 13:36:42 -0500

    Leverage the stx-update, stx-integ and stx-fault DevStack plugins
    sysinv needs:
    * stx-integ: platform-utils
    * stx-update: sw-patch, tsconfig
    * stx-fault: fm-api, fm-common
    
    Depends-On: https://review.openstack.org/614804
    Depends-On: https://review.openstack.org/#/c/613973/
    Depends-On: https://review.openstack.org/#/c/614244/
    Change-Id: I85b49a954300907d6693df9d18eb37296d6f3f0c
    Signed-off-by: Dean Troyer <dtroyer@gmail.com>

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -3,36 +3,27 @@
 # devstack/plugin.sh
 # Triggers stx_config specific functions to install and configure stx_config
 
-# Dependencies:
-#
-# - ``functions`` file
-# - ``DATA_DIR`` must be defined
-
-# ``stack.sh`` calls the entry points in this order:
-#
 echo_summary "sysinv devstack plugin.sh called: $1/$2"
-source $DEST/stx-config/devstack/lib/stx-config
-# check for service enabled
 
-if is_service_enabled sysinv-api sysinv-cond; then
+# check for service enabled
+if is_service_enabled stx-config; then
     if [[ "$1" == "stack" && "$2" == "install" ]]; then
-        # Perform installation of sysinv source
+        # Perform installation of source
         echo_summary "Installing cgts_client"
         install_cgtsclient
         echo_summary "Installing depends"
         install_sysinv_depends
         echo_summary "Installing sysinv service"
         install_sysinv
-
     elif [[ "$1" == "stack" && "$2" == "post-config" ]]; then
         # Configure after the other layer 1 and 2 services have been configured
-        echo_summary "Configuring sysinv"
+        echo_summary "Configure sysinv"
         configure_sysinv
         create_sysinv_user_group
         create_sysinv_accounts
     elif [[ "$1" == "stack" && "$2" == "extra" ]]; then
         # Initialize and start the sysinv service
-        echo_summary "Initializing and start sysinv "
+        echo_summary "Initialize and start sysinv "
         init_sysinv
         start_sysinv
     elif [[ "$1" == "stack" && "$2" == "test-config" ]]; then
@@ -44,11 +35,9 @@ if is_service_enabled sysinv-api sysinv-cond; then
         # Shut down sysinv services
         echo_summary "Stop Sysinv service"
         stop_sysinv
-        :
     fi
 
     if [[ "$1" == "clean" ]]; then
         cleanup_sysinv
-        :
     fi
 fi
