9a707a3402ef6363f41cc13cd949fcd5c42e6d5f
Robert Church <robert.church@windriver.com>
2018-10-10 23:13:36 -0400

    Updates to the existing Ingress overrides
    For integration purposes, set the replicas based on the number of
    controllers present in the install base.
    
    Change-Id: I75ed25ecc886ae9796047cdfe7a7883800f0f60c
    Story: 2003909
    Task: 27086
    Signed-off-by: Robert Church <robert.church@windriver.com>

--- sysinv/sysinv/sysinv/sysinv/helm/ingress.py
+++ sysinv/sysinv/sysinv/sysinv/helm/ingress.py
@@ -8,12 +8,12 @@ from sysinv.common import constants
 from sysinv.common import exception
 from sysinv.openstack.common import log as logging
 from . import common
-from . import openstack
+from . import base
 
 LOG = logging.getLogger(__name__)
 
 
-class IngressHelm(openstack.OpenstackBaseHelm):
+class IngressHelm(base.BaseHelm):
     """Class to encapsulate helm operations for the ingress chart"""
 
     CHART = constants.HELM_CHART_INGRESS
@@ -29,6 +29,11 @@ class IngressHelm(openstack.OpenstackBaseHelm):
         # Currently have conflicts with ports 80 and 8080, use 8081 for now
         overrides = {
             common.HELM_NS_KUBE_SYSTEM: {
+                'pod': {
+                    'replicas': {
+                        'error_page': self._num_controllers()
+                    }
+                },
                 'deployment': {
                     'mode': 'cluster',
                     'type': 'DaemonSet'
@@ -47,6 +52,12 @@ class IngressHelm(openstack.OpenstackBaseHelm):
                 }
             },
             common.HELM_NS_OPENSTACK: {
+                'pod': {
+                    'replicas': {
+                        'ingress': self._num_controllers(),
+                        'error_page': self._num_controllers()
+                    }
+                }
             }
         }
 
