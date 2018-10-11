96d086c5ffa0053d31965f4787dd554b24e665d6
Robert Church <robert.church@windriver.com>
2018-10-10 23:05:21 -0400

    Introduce support for Barbican chart overrides
    Add plugin with override support for:
     - replicas based on the number of controllers
     - docker image source
    
    Change-Id: I52405d352dceed9694c181f0437f4e5020b329f2
    Story: 2003909
    Task: 27087
    Signed-off-by: Robert Church <robert.church@windriver.com>

---
+++ sysinv/sysinv/sysinv/sysinv/helm/barbican.py
@@ -0,0 +1,61 @@
+#
+# Copyright (c) 2018 Wind River Systems, Inc.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from sysinv.common import constants
+from sysinv.common import exception
+from . import common
+from . import openstack
+
+
+class BarbicanHelm(openstack.OpenstackBaseHelm):
+    """Class to encapsulate helm operations for the barbican chart"""
+
+    CHART = constants.HELM_CHART_BARBICAN
+    SUPPORTED_NAMESPACES = [
+        common.HELM_NS_OPENSTACK
+    ]
+
+    SERVICE_NAME = constants.HELM_CHART_BARBICAN
+
+    def get_namespaces(self):
+        return self.SUPPORTED_NAMESPACES
+
+    def get_overrides(self, namespace=None):
+        overrides = {
+            common.HELM_NS_OPENSTACK: {
+                'images': self._get_images_overrides(),
+                'pod': {
+                    'replicas': {
+                        'api': self._num_controllers()
+                    }
+                }
+            }
+        }
+
+        if namespace in self.SUPPORTED_NAMESPACES:
+            return overrides[namespace]
+        elif namespace:
+            raise exception.InvalidHelmNamespace(chart=self.CHART,
+                                                 namespace=namespace)
+        else:
+            return overrides
+
+    def _get_images_overrides(self):
+        heat_image = self._operator.chart_operators[
+            constants.HELM_CHART_HEAT].docker_image
+        return {
+            'tags': {
+                'barbican_api': self.docker_image,
+                'barbican_db_sync': self.docker_image,
+                'bootstrap': heat_image,
+                'db_drop': heat_image,
+                'db_init': heat_image,
+                'ks_endpoints': heat_image,
+                'ks_service': heat_image,
+                'ks_user': heat_image,
+                'scripted_test': heat_image,
+            }
+        }
