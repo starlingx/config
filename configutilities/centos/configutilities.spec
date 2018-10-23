openstack-archive/stx-config

Add hooks for python wheel generation
This update adds hooks to the spec files for the following packages
to generate wheels for the python modules:
- configutilities
- controllerconfig
- cgts-client
- sysinv

Change-Id: I340db4c6516c9bf8badbdd0ecdcf61670e168aea
Story: 2003907
Task: 27524
Signed-off-by: Don Penney <don.penney@windriver.com>

configutilities/centos/configutilities.spec
@@ -12,6 +12,8 @@ Source1: LICENSE
 %define debug_package %{nil}
 
 BuildRequires: python-setuptools
+BuildRequires: python2-pip
+BuildRequires: python2-wheel
 Requires: python-netaddr
 #Requires: wxPython
 
@@ -35,6 +37,7 @@ SDK files for configutilities
 
 %build
 %{__python} setup.py build
+%py2_build_wheel
 
 %install
 %{__python} setup.py install --root=$RPM_BUILD_ROOT \
@@ -42,6 +45,8 @@ SDK files for configutilities
                              --prefix=/usr \
                              --install-data=/usr/share \
                              --single-version-externally-managed
+mkdir -p $RPM_BUILD_ROOT/wheels
+install -m 644 dist/*.whl $RPM_BUILD_ROOT/wheels/
 
 sed -i "s#xxxSW_VERSIONxxx#%{platform_release}#" %{name}/common/validator.py
 tar czf %{cgcs_sdk_tarball_name} %{name}
@@ -62,3 +67,12 @@ rm -rf $RPM_BUILD_ROOT
 
 %files -n %{name}-cgts-sdk
 %{cgcs_sdk_deploy_dir}/%{cgcs_sdk_tarball_name}
+
+%package wheels
+Summary: %{module_name} wheels
+
+%description wheels
+Contains python wheels for %{module_name}
+
+%files wheels
+/wheels/*

controllerconfig/centos/controllerconfig.spec
@@ -9,6 +9,8 @@ URL: unknown
 Source0: %{name}-%{version}.tar.gz
 
 BuildRequires: python-setuptools
+BuildRequires: python2-pip
+BuildRequires: python2-wheel
 Requires: systemd
 Requires: python-netaddr
 Requires: python-keyring
@@ -36,6 +38,7 @@ Controller node configuration
 
 %build
 %{__python} setup.py build
+%py2_build_wheel
 
 # TODO: NO_GLOBAL_PY_DELETE (see python-byte-compile.bbclass), put in macro/script
 %install
@@ -44,6 +47,8 @@ Controller node configuration
                              --prefix=/usr \
                              --install-data=/usr/share \
                              --single-version-externally-managed
+mkdir -p $RPM_BUILD_ROOT/wheels
+install -m 644 dist/*.whl $RPM_BUILD_ROOT/wheels/
 
 install -d -m 755 %{buildroot}%{local_bindir}
 install -p -D -m 700 scripts/keyringstaging %{buildroot}%{local_bindir}/keyringstaging
@@ -84,3 +89,12 @@ rm -rf $RPM_BUILD_ROOT
 %dir %{local_etc_upgraded}
 %{local_etc_upgraded}/*
 %{local_etc_systemd}/*
+
+%package wheels
+Summary: %{module_name} wheels
+
+%description wheels
+Contains python wheels for %{module_name}
+
+%files wheels
+/wheels/*

sysinv/sysinv/centos/sysinv.spec
@@ -10,6 +10,8 @@ Source0: %{name}-%{version}.tar.gz
 
 BuildRequires: python-setuptools
 BuildRequires: python-pbr
+BuildRequires: python2-pip
+BuildRequires: python2-wheel
 Requires: python-pyudev
 Requires: pyparted
 Requires: python-ipaddr
@@ -39,6 +41,7 @@ rm -rf *.egg-info
 %build
 export PBR_VERSION=%{version}
 %{__python} setup.py build
+%py2_build_wheel
 
 %install
 export PBR_VERSION=%{version}
@@ -47,6 +50,8 @@ export PBR_VERSION=%{version}
                              --prefix=/usr \
                              --install-data=/usr/share \
                              --single-version-externally-managed
+mkdir -p $RPM_BUILD_ROOT/wheels
+install -m 644 dist/*.whl $RPM_BUILD_ROOT/wheels/
 
 install -d -m 755 %{buildroot}%{local_etc_goenabledd}
 install -p -D -m 755 etc/sysinv/sysinv_goenabled_check.sh %{buildroot}%{local_etc_goenabledd}/sysinv_goenabled_check.sh
@@ -116,3 +121,12 @@ rm -rf $RPM_BUILD_ROOT
 %{_bindir}/sysinv-upgrade
 %{_bindir}/sysinv-puppet
 %{_bindir}/sysinv-helm
+
+%package wheels
+Summary: %{module_name} wheels
+
+%description wheels
+Contains python wheels for %{module_name}
+
+%files wheels
+/wheels/*

starlingx/config

configutilities/centos/configutilities.spec
@@ -12,6 +12,8 @@ Source1: LICENSE
 %define debug_package %{nil}
 
 BuildRequires: python-setuptools
+BuildRequires: python2-pip
+BuildRequires: python2-wheel
 Requires: python-netaddr
 #Requires: wxPython
 
@@ -35,6 +37,7 @@ SDK files for configutilities
 
 %build
 %{__python} setup.py build
+%py2_build_wheel
 
 %install
 %{__python} setup.py install --root=$RPM_BUILD_ROOT \
@@ -42,6 +45,8 @@ SDK files for configutilities
                              --prefix=/usr \
                              --install-data=/usr/share \
                              --single-version-externally-managed
+mkdir -p $RPM_BUILD_ROOT/wheels
+install -m 644 dist/*.whl $RPM_BUILD_ROOT/wheels/
 
 sed -i "s#xxxSW_VERSIONxxx#%{platform_release}#" %{name}/common/validator.py
 tar czf %{cgcs_sdk_tarball_name} %{name}
@@ -62,3 +67,12 @@ rm -rf $RPM_BUILD_ROOT
 
 %files -n %{name}-cgts-sdk
 %{cgcs_sdk_deploy_dir}/%{cgcs_sdk_tarball_name}
+
+%package wheels
+Summary: %{name} wheels
+
+%description wheels
+Contains python wheels for %{name}
+
+%files wheels
+/wheels/*

controllerconfig/centos/controllerconfig.spec
@@ -9,6 +9,8 @@ URL: unknown
 Source0: %{name}-%{version}.tar.gz
 
 BuildRequires: python-setuptools
+BuildRequires: python2-pip
+BuildRequires: python2-wheel
 Requires: systemd
 Requires: python-netaddr
 Requires: python-keyring
@@ -36,6 +38,7 @@ Controller node configuration
 
 %build
 %{__python} setup.py build
+%py2_build_wheel
 
 # TODO: NO_GLOBAL_PY_DELETE (see python-byte-compile.bbclass), put in macro/script
 %install
@@ -44,6 +47,8 @@ Controller node configuration
                              --prefix=/usr \
                              --install-data=/usr/share \
                              --single-version-externally-managed
+mkdir -p $RPM_BUILD_ROOT/wheels
+install -m 644 dist/*.whl $RPM_BUILD_ROOT/wheels/
 
 install -d -m 755 %{buildroot}%{local_bindir}
 install -p -D -m 700 scripts/keyringstaging %{buildroot}%{local_bindir}/keyringstaging
@@ -84,3 +89,12 @@ rm -rf $RPM_BUILD_ROOT
 %dir %{local_etc_upgraded}
 %{local_etc_upgraded}/*
 %{local_etc_systemd}/*
+
+%package wheels
+Summary: %{name} wheels
+
+%description wheels
+Contains python wheels for %{name}
+
+%files wheels
+/wheels/*

sysinv/sysinv/centos/sysinv.spec
@@ -10,6 +10,8 @@ Source0: %{name}-%{version}.tar.gz
 
 BuildRequires: python-setuptools
 BuildRequires: python-pbr
+BuildRequires: python2-pip
+BuildRequires: python2-wheel
 Requires: python-pyudev
 Requires: pyparted
 Requires: python-ipaddr
@@ -39,6 +41,7 @@ rm -rf *.egg-info
 %build
 export PBR_VERSION=%{version}
 %{__python} setup.py build
+%py2_build_wheel
 
 %install
 export PBR_VERSION=%{version}
@@ -47,6 +50,8 @@ export PBR_VERSION=%{version}
                              --prefix=/usr \
                              --install-data=/usr/share \
                              --single-version-externally-managed
+mkdir -p $RPM_BUILD_ROOT/wheels
+install -m 644 dist/*.whl $RPM_BUILD_ROOT/wheels/
 
 install -d -m 755 %{buildroot}%{local_etc_goenabledd}
 install -p -D -m 755 etc/sysinv/sysinv_goenabled_check.sh %{buildroot}%{local_etc_goenabledd}/sysinv_goenabled_check.sh
@@ -116,3 +121,12 @@ rm -rf $RPM_BUILD_ROOT
 %{_bindir}/sysinv-upgrade
 %{_bindir}/sysinv-puppet
 %{_bindir}/sysinv-helm
+
+%package wheels
+Summary: %{name} wheels
+
+%description wheels
+Contains python wheels for %{name}
+
+%files wheels
+/wheels/*