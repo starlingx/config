openstack-archive/stx-config

Introduce support for panko chart overrides
Add panko helm chart overrides support for:
 - docker image source
 - user passwords

Change-Id: I03fb21394846ecf5dd312074aeec68f8bdf24238
Story: 2003909
Task: 27083
Signed-off-by: Angie Wang <angie.wang@windriver.com>

sysinv/sysinv/sysinv/sysinv/helm/panko.py
@@ -1,0 +1,82 @@
+#
+# Copyright (c) 2018 Wind River Systems, Inc.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from sysinv.common import constants
+from sysinv.common import exception
+from sysinv.openstack.common import log as logging
+from . import common
+from . import openstack
+
+LOG = logging.getLogger(__name__)
+
+
+class PankoHelm(openstack.OpenstackBaseHelm):
+    """Class to encapsulate helm operations for the panko chart"""
+
+    CHART = constants.HELM_CHART_GNOCCHI
+    SUPPORTED_NAMESPACES = [
+        common.HELM_NS_OPENSTACK
+    ]
+
+    SERVICE_NAME = 'panko'
+    AUTH_USERS = ['panko']
+
+    @property
+    def docker_repo_source(self):
+        return common.DOCKER_SRC_STX
+
+    def get_namespaces(self):
+        return self.SUPPORTED_NAMESPACES
+
+    def get_overrides(self, namespace=None):
+        overrides = {
+            common.HELM_NS_OPENSTACK: {
+                'images': self._get_images_overrides(),
+                'endpoints': self._get_endpoints_overrides()
+            }
+        }
+
+        if namespace in self.SUPPORTED_NAMESPACES:
+            return overrides[namespace]
+        elif namespace:
+            raise exception.InvalidHelmNamespace(chart=self.CHART,
+                                                 namespace=namespace)
+        else:
+            return overrides
+
+    def _get_images_overrides(self):
+        heat_image = self._operator.chart_operators[
+            constants.HELM_CHART_HEAT].docker_image
+        return {
+            'tags': {
+                'db_init': self.docker_image,
+                'db_sync': self.docker_image,
+                'panko_api': self.docker_image,
+                'panko_events_cleaner': self.docker_image,
+                'ks_endpoints': heat_image,
+                'ks_service': heat_image,
+                'ks_user': heat_image,
+            }
+        }
+
+    def _get_endpoints_overrides(self):
+        return {
+            'identity': {
+                'auth':
+                self._get_endpoints_identity_overrides(
+                    self.SERVICE_NAME, self.AUTH_USERS),
+            },
+            'oslo_db': {
+                'auth': self._get_endpoints_oslo_db_overrides(
+                    self.SERVICE_NAME, self.AUTH_USERS)
+            },
+            'oslo_cache': {
+                'auth': {
+                    'memcached_secret_key':
+                        self._get_common_password('auth_memcache_key')
+                }
+            },
+        }

starlingx/config

sysinv/sysinv/sysinv/sysinv/helm/panko.py
@@ -1,0 +1,84 @@
+#
+# Copyright (c) 2018 Wind River Systems, Inc.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from sysinv.common import constants
+from sysinv.common import exception
+from sysinv.openstack.common import log as logging
+from . import common
+from . import openstack
+
+LOG = logging.getLogger(__name__)
+
+
+class PankoHelm(openstack.OpenstackBaseHelm):
+    """Class to encapsulate helm operations for the panko chart"""
+
+    CHART = constants.HELM_CHART_PANKO
+    SUPPORTED_NAMESPACES = [
+        common.HELM_NS_OPENSTACK
+    ]
+
+    SERVICE_NAME = 'panko'
+    AUTH_USERS = ['panko']
+
+    @property
+    def docker_repo_source(self):
+        return common.DOCKER_SRC_STX
+
+    def get_namespaces(self):
+        return self.SUPPORTED_NAMESPACES
+
+    def get_overrides(self, namespace=None):
+        overrides = {
+            common.HELM_NS_OPENSTACK: {
+                'images': self._get_images_overrides(),
+                'endpoints': self._get_endpoints_overrides()
+            }
+        }
+
+        if namespace in self.SUPPORTED_NAMESPACES:
+            return overrides[namespace]
+        elif namespace:
+            raise exception.InvalidHelmNamespace(chart=self.CHART,
+                                                 namespace=namespace)
+        else:
+            return overrides
+
+    def _get_images_overrides(self):
+        heat_image = self._operator.chart_operators[
+            constants.HELM_CHART_HEAT].docker_image
+        return {
+            'tags': {
+                'bootstrap': heat_image,
+                'db_drop': heat_image,
+                'db_init': self.docker_image,
+                'db_sync': self.docker_image,
+                'panko_api': self.docker_image,
+                'panko_events_cleaner': self.docker_image,
+                'ks_endpoints': heat_image,
+                'ks_service': heat_image,
+                'ks_user': heat_image,
+            }
+        }
+
+    def _get_endpoints_overrides(self):
+        return {
+            'identity': {
+                'auth':
+                self._get_endpoints_identity_overrides(
+                    self.SERVICE_NAME, self.AUTH_USERS),
+            },
+            'oslo_db': {
+                'auth': self._get_endpoints_oslo_db_overrides(
+                    self.SERVICE_NAME, self.AUTH_USERS)
+            },
+            'oslo_cache': {
+                'auth': {
+                    'memcached_secret_key':
+                        self._get_common_password('auth_memcache_key')
+                }
+            },
+        }