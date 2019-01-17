e4086a073b7f661d4014061f25525120c27ea679
Mingyuan Qi <mingyuan.qi@intel.com>
2019-01-17 22:42:50 +0800

    Add docker proxy configuration for kubernetes
    Current config_controller for containerization pulls kubernetes
    images from public container registry. This requires controller
    to access internet. If a host network is behind a proxy, there
    is no chance to setup docker proxy configuration during config_
    controller, therefore, kubernetes images are not accessible.
    
    Docker proxy configuration questions were added to require users
    input http/https proxy and no proxy settings. The docker proxy
    configurations are added to service_parameter table in sysinv.
    
    http-proxy.conf is the proxy info file required by docker daemon,
    generated by docker puppet manifest. It consists of the user
    input docker proxy configuration.
    
    Tests:
    AIO-SX: public k8s images accessible
    AIO-DX: public k8s images accessible
    AIO-SX without k8s config: config_controller successfully
    
    Story: 2004710
    Task: 28741
    
    Change-Id: I9d301d105f9fe5e388a29dee03957514b9b44fa7
    Signed-off-by: Mingyuan Qi <mingyuan.qi@intel.com>

--- puppet-manifests/src/modules/platform/manifests/docker.pp
+++ puppet-manifests/src/modules/platform/manifests/docker.pp
@@ -1,5 +1,8 @@
 class platform::docker::params (
   $package_name    = 'docker-ce',
+  $http_proxy   = undef,
+  $https_proxy  = undef,
+  $no_proxy     = undef,
 ) { }
 
 class platform::docker::config
@@ -8,6 +11,22 @@ class platform::docker::config
   include ::platform::kubernetes::params
 
   if $::platform::kubernetes::params::enabled {
+
+    if $http_proxy or $https_proxy {
+      file { '/etc/systemd/system/docker.service.d':
+        ensure => 'directory',
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0755',
+      }
+      -> file { '/etc/systemd/system/docker.service.d/http-proxy.conf':
+        ensure  => present,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0644',
+        content => template('platform/dockerproxy.conf.erb'),
+      }
+    }
 
     Class['::platform::filesystem::docker'] ~> Class[$name]
 
