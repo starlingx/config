openstack-archive/stx-config

Create DataNetworks modelling in System Configuration
Introduce the DataNetwork api, client, model to allow modelling of the
physical data network, with the following attributes:
   datanetwork_name
   mtu
   datanetwork_type (flat, vlan, vxlan)
   VxLAN specific attributes
      - port_number
      - multicast_group
      - ttl
      - mode ('dynamic' default, or 'static')

The system data network may then be assigned to interface.

This is part of the Story to "Move neutron provider network modelling
to system configuration".

The interface api is currently made compatible with current usage to
allow specifying datanetwork (formerly providernetwork).

The following new CLI commands and corresponding api are exposed:
    datanetwork-add     Add a datanetwork.
    datanetwork-delete  Delete a datanetwork.
    datanetwork-list    List datanetworks.
    datanetwork-modify  Modify a datanetwork.
    datanetwork-show    Show datanetwork details.
    interface-datanetwork-assign
                       Assign a datanetwork to an interface.
    interface-datanetwork-list
                       List datanetwork interfaces.
    interface-datanetwork-remove
                       Remove an assigned datanetwork from an interface.
    interface-datanetwork-show
                       Show interface datanetwork details.

'system datanetwork-add' must be run where
    'neutron providernetwork-create'.

Tests Performed:
    AIO Sanity
    Sanity 2-controller, 2-compute
    Interface Profile create and apply
    Containers deployment

Change-Id: I630f90768647dbb414a60978bf8f8f641496afd5
Story: 2004455
Task: 28324
Signed-off-by: John Kung <john.kung@windriver.com>

sysinv/cgts-client/cgts-client/cgtsclient/v1/datanetwork_shell.py
@@ -1,0 +1,130 @@
+#!/usr/bin/env python
+#
+# Copyright (c) 2019 Wind River Systems, Inc.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+# All Rights Reserved.
+#
+
+from cgtsclient.common import utils
+from cgtsclient import exc
+
+DATANETWORK_TYPE_VXLAN = "vxlan"
+
+
+def _print_datanetwork_show(obj):
+    fields = ['id', 'uuid', 'name', 'datanetwork_type', 'mtu',
+              'description']
+
+    if obj.datanetwork_type == DATANETWORK_TYPE_VXLAN:
+        fields.append('multicast_group')
+        fields.append('port_num')
+        fields.append('ttl')
+        fields.append('mode')
+
+    data = [(f, getattr(obj, f, '')) for f in fields]
+    utils.print_tuple_list(data)
+
+
+@utils.arg('datanetwork_id',
+           metavar='<datanetwork_id>',
+           help="UUID or name of datanetwork")
+def do_datanetwork_show(cc, args):
+    """Show datanetwork details."""
+
+    datanetwork = cc.datanetwork.get(args.datanetwork_id)
+    _print_datanetwork_show(datanetwork)
+
+
+def do_datanetwork_list(cc, args):
+    """List datanetworks."""
+
+    labels = ['uuid', 'name', 'datanetwork_type', 'mtu']
+    fields = ['uuid', 'name', 'datanetwork_type', 'mtu']
+    datanetworks = cc.datanetwork.list()
+    utils.print_list(datanetworks, fields, labels, sortby=1)
+
+
+@utils.arg('name',
+           metavar='<datanetwork_name>',
+           help="Name of the datanetwork [REQUIRED]")
+@utils.arg('datanetwork_type',
+           metavar='<datanetwork_type>',
+           choices=['flat', 'vlan', 'vxlan'],
+           help="Type of the datanetwork [REQUIRED]")
+@utils.arg('-d', '--description',
+           metavar='<description>',
+           help='User description of the datanetwork')
+@utils.arg('-m', '--mtu',
+           metavar='<mtu>',
+           default=1500,
+           help='MTU of the datanetwork')
+@utils.arg('-p', '--port_num',
+           metavar='<port_num>',
+           help='port_num of the datanetwork')
+@utils.arg('-g', '--multicast_group',
+           metavar='<multicast_group>',
+           help='multicast_group of the datanetwork')
+@utils.arg('-t', '--ttl',
+           metavar='<ttl>',
+           help='time-to-live of the datanetwork')
+@utils.arg('-M', '--mode',
+           metavar='<mode>',
+           choices=['dynamic', 'static'],
+           default='dynamic',
+           help='mode of the datanetwork')
+def do_datanetwork_add(cc, args):
+    """Add a datanetwork."""
+
+    field_list = ['name', 'datanetwork_type', 'mtu', 'description',
+                  'multicast_group', 'port_num', 'ttl', 'mode']
+
+    # Prune input fields down to required/expected values
+    data = dict((k, v) for (k, v) in vars(args).items()
+                if k in field_list and not (v is None))
+
+    datanetwork = cc.datanetwork.create(**data)
+    uuid = getattr(datanetwork, 'uuid', '')
+    try:
+        datanetwork = cc.datanetwork.get(uuid)
+    except exc.HTTPNotFound:
+        raise exc.CommandError('Created DataNetwork UUID not found: %s' % uuid)
+    _print_datanetwork_show(datanetwork)
+
+
+@utils.arg('datanetwork_id',
+           metavar='<datanetwork_id>',
+           help="Name of the datanetwork [REQUIRED]")
+@utils.arg('-m', '--mtu',
+           metavar='<mtu>',
+           default=1500,
+           help='MTU of the datanetwork')
+@utils.arg('-d', '--description',
+           metavar='<description>',
+           help='User description of the datanetwork')
+def do_datanetwork_modify(cc, args):
+    """Modify a datanetwork."""
+
+    rwfields = ['mtu', 'description']
+
+    user_specified_fields = dict((k, v) for (k, v) in vars(args).items()
+                                 if k in rwfields and not (v is None))
+
+    patch = []
+    for (k, v) in user_specified_fields.items():
+        patch.append({'op': 'replace', 'path': '/' + k, 'value': v})
+
+    datanetwork = cc.datanetwork.update(args.datanetwork_id, patch)
+    _print_datanetwork_show(datanetwork)
+
+
+@utils.arg('datanetwork_uuid',
+           metavar='<datanetwork_uuid>',
+           help="UUID of datanetwork entry")
+def do_datanetwork_delete(cc, args):
+    """Delete a datanetwork."""
+
+    cc.datanetwork.delete(args.datanetwork_uuid)
+    print('Deleted DataNetwork: %s' % args.datanetwork_uuid)

sysinv/sysinv/sysinv/sysinv/common/exception.py
@@ -1293,3 +1293,57 @@ class LocalManagementIpNotFound(NotFound):
 
 class InvalidHelmDockerImageSource(Invalid):
     message = _("Invalid docker image source: %(source)s. Must be one of %(valid_srcs)s")
+
+
+# DataNetwork
+class UnsupportedInterfaceDataNetworkType(Conflict):
+    message = _("Interface with datanetwork type '%(datanetworktype)s' "
+                "is not supported.")
+
+
+class DataNetworkNotFound(NotFound):
+    message = _("DataNetwork %(datanetwork_uuid)s could not be found.")
+
+
+class DataNetworkTypeNotFound(NotFound):
+    message = _("DataNetwork of type %(datanetwork_type)s could not be found.")
+
+
+class DataNetworkIDNotFound(NotFound):
+    message = _("DataNetwork with id %(id)s could not be found.")
+
+
+class DataNetworkNameNotFound(NotFound):
+    message = _("DataNetwork with name %(name)s could not be found.")
+
+
+class DataNetworkAlreadyExists(Conflict):
+    message = _("DataNetwork of name %(name)s already exists.")
+
+
+class DataNetworkTypeUnsupported(Conflict):
+    message = _("DataNetwork of type %(datanetwork_type)s is not supported.")
+
+
+class InterfaceDataNetworkNotFound(NotFound):
+    message = _("Interface datanetwork %(uuid)s could not be found.")
+
+
+class InterfaceDataNetworkAlreadyExists(Conflict):
+    message = _("Interface datanetwork with interface ID %(interface_id)s "
+                "and datanetwork ID %(datanetwork_id)s already exists.")
+
+
+class InterfaceDataNetworkNotFoundByKeys(NotFound):
+    message = _("Interface datanetwork with interface ID %(interface_id)s "
+                "and datanetwork ID %(datanetwork_id)s not found")
+
+
+class UnsupportedAssignedInterfaceDataNetworkType(Conflict):
+    message = _("Cannot assign datanetwork with type '%(datanetwork_type)s' "
+                "to an interface.")
+
+
+class UnsupportedRemovedInterfaceDataNetworkType(Conflict):
+    message = _("Cannot remove datanetwork with type '%(datanetwork_type)s' "
+                "from an interface.")

sysinv/sysinv/sysinv/sysinv/objects/datanetwork.py
@@ -1,0 +1,42 @@
+#
+# Copyright (c) 2019 Wind River Systems, Inc.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+# coding=utf-8
+#
+
+from sysinv.db import api as db_api
+from sysinv.objects import base
+from sysinv.objects import utils
+
+
+class DataNetwork(base.SysinvObject):
+    VERSION = '1.0'
+
+    dbapi = db_api.get_instance()
+
+    fields = {'id': int,
+              'uuid': utils.uuid_or_none,
+              'datanetwork_type': utils.str_or_none,
+              'name': utils.str_or_none,
+              'description': utils.str_or_none,
+              'mtu': utils.int_or_none,
+              'multicast_group': utils.str_or_none,
+              'port_num': utils.int_or_none,
+              'ttl': utils.int_or_none,
+              'mode': utils.str_or_none,
+              }
+
+    _optional_fields = {'port_num',
+                        'multicast_group',
+                        'ttl',
+                        'mode'}
+
+    @base.remotable_classmethod
+    def get_by_uuid(cls, context, uuid):
+        return cls.dbapi.datanetwork_get(uuid)
+
+    def save_changes(self, context, updates):
+        self.dbapi.datanetwork_update(self.uuid, updates)

sysinv/sysinv/sysinv/sysinv/puppet/interface.py
@@ -122,6 +122,7 @@ class InterfacePuppet(base.BasePuppet):
             'system_mode': self._get_system().system_mode,
             'ports': self._get_port_interface_id_index(host),
             'interfaces': self._get_interface_name_index(host),
+            'interfaces_datanets': self._get_interface_name_datanets(host),
             'devices': self._get_port_pciaddr_index(host),
             'addresses': self._get_address_interface_name_index(host),
             'routes': self._get_routes_interface_name_index(host),
@@ -129,6 +130,7 @@ class InterfacePuppet(base.BasePuppet):
             'gateways': self._get_gateway_index(),
             'floatingips': self._get_floating_ip_index(),
             'providernets': self._get_provider_networks(host),
+            'datanets': self._get_datanetworks(host),
         }
         return context
 
@@ -160,6 +162,41 @@ class InterfacePuppet(base.BasePuppet):
         interfaces = {}
         for iface in self.dbapi.iinterface_get_by_ihost(host.id):
             interfaces[iface.ifname] = iface
+
+        return interfaces
+
+    def _get_interface_name_datanets(self, host):
+        """
+        Builds a dictionary of datanets indexed by interface name.
+        """
+        interfaces = {}
+        for iface in self.dbapi.iinterface_get_by_ihost(host.id):
+            ifdatanets = self.dbapi.interface_datanetwork_get_by_interface(
+                iface.uuid)
+
+            datanetworks = []
+            for ifdatanet in ifdatanets:
+                datanetworks.append(ifdatanet.datanetwork_uuid)
+
+            datanetworks_list = []
+            for datanetwork in datanetworks:
+                dn = self.dbapi.datanetwork_get(datanetwork)
+                datanetwork_dict = \
+                    {'name': dn.name,
+                     'uuid': dn.uuid,
+                     'datanetwork_type': dn.datanetwork_type,
+                     'mtu': dn.mtu}
+                if dn.datanetwork_type == constants.DATANETWORK_TYPE_VXLAN:
+                    datanetwork_dict.update(
+                        {'multicast_group': dn.multicast_group,
+                         'port_num': dn.port_num,
+                         'ttl': dn.ttl,
+                         'mode': dn.mode})
+                datanetworks_list.append(datanetwork_dict)
+            interfaces[iface.ifname] = datanetworks_list
+
+        LOG.debug("_get_interface_name_datanets ifdatanet=%s" % interfaces)
+
         return interfaces
 
     def _get_port_pciaddr_index(self, host):
@@ -288,6 +325,12 @@ class InterfacePuppet(base.BasePuppet):
                 constants.WORKER in utils.get_personalities(host)):
             pnets = self.openstack.get_providernetworksdict(quiet=True)
         return pnets
+
+    def _get_datanetworks(self, host):
+        dnets = {}
+        if constants.WORKER in utils.get_personalities(host):
+            dnets = self.dbapi.datanetworks_get_all()
+        return dnets
 
 
 def is_platform_network_type(iface):
@@ -469,6 +512,13 @@ def get_interface_providernets(iface):
     if not providernetworks:
         return []
     return [x.strip() for x in providernetworks.split(',')]
+
+
+def get_interface_datanets(context, iface):
+    """
+    Return the list of data networks of the supplied interface
+    """
+    return context['interfaces_datanets'][iface.ifname]
 
 
 def get_interface_port(context, iface):

starlingx/config

    Sanity Storage lab

sysinv/cgts-client/cgts-client/cgtsclient/v1/datanetwork_shell.py
@@ -1,0 +1,129 @@
+#!/usr/bin/env python
+#
+# Copyright (c) 2019 Wind River Systems, Inc.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+# All Rights Reserved.
+#
+
+from cgtsclient.common import utils
+from cgtsclient import exc
+
+DATANETWORK_TYPE_VXLAN = "vxlan"
+
+
+def _print_datanetwork_show(obj):
+    fields = ['id', 'uuid', 'name', 'network_type', 'mtu',
+              'description']
+
+    if obj.network_type == DATANETWORK_TYPE_VXLAN:
+        fields.append('multicast_group')
+        fields.append('port_num')
+        fields.append('ttl')
+        fields.append('mode')
+
+    data = [(f, getattr(obj, f, '')) for f in fields]
+    utils.print_tuple_list(data)
+
+
+@utils.arg('datanetwork_id',
+           metavar='<datanetwork_id>',
+           help="UUID or name of datanetwork")
+def do_datanetwork_show(cc, args):
+    """Show datanetwork details."""
+
+    datanetwork = cc.datanetwork.get(args.datanetwork_id)
+    _print_datanetwork_show(datanetwork)
+
+
+def do_datanetwork_list(cc, args):
+    """List datanetworks."""
+
+    labels = ['uuid', 'name', 'network_type', 'mtu']
+    fields = ['uuid', 'name', 'network_type', 'mtu']
+    datanetworks = cc.datanetwork.list()
+    utils.print_list(datanetworks, fields, labels, sortby=1)
+
+
+@utils.arg('name',
+           metavar='<datanetwork_name>',
+           help="Name of the datanetwork [REQUIRED]")
+@utils.arg('network_type',
+           metavar='<network_type>',
+           choices=['flat', 'vlan', 'vxlan'],
+           help="Type of the datanetwork [REQUIRED]")
+@utils.arg('-d', '--description',
+           metavar='<description>',
+           help='User description of the datanetwork')
+@utils.arg('-m', '--mtu',
+           metavar='<mtu>',
+           default=1500,
+           help='MTU of the datanetwork')
+@utils.arg('-p', '--port_num',
+           metavar='<port_num>',
+           help='port_num of the datanetwork')
+@utils.arg('-g', '--multicast_group',
+           metavar='<multicast_group>',
+           help='multicast_group of the datanetwork')
+@utils.arg('-t', '--ttl',
+           metavar='<ttl>',
+           help='time-to-live of the datanetwork')
+@utils.arg('-M', '--mode',
+           metavar='<mode>',
+           choices=['dynamic', 'static'],
+           default='dynamic',
+           help='mode of the datanetwork')
+def do_datanetwork_add(cc, args):
+    """Add a datanetwork."""
+
+    field_list = ['name', 'network_type', 'mtu', 'description',
+                  'multicast_group', 'port_num', 'ttl', 'mode']
+
+    # Prune input fields down to required/expected values
+    data = dict((k, v) for (k, v) in vars(args).items()
+                if k in field_list and not (v is None))
+
+    datanetwork = cc.datanetwork.create(**data)
+    uuid = getattr(datanetwork, 'uuid', '')
+    try:
+        datanetwork = cc.datanetwork.get(uuid)
+    except exc.HTTPNotFound:
+        raise exc.CommandError('Created DataNetwork UUID not found: %s' % uuid)
+    _print_datanetwork_show(datanetwork)
+
+
+@utils.arg('datanetwork_id',
+           metavar='<datanetwork_id>',
+           help="Name of the datanetwork [REQUIRED]")
+@utils.arg('-m', '--mtu',
+           metavar='<mtu>',
+           help='MTU of the datanetwork')
+@utils.arg('-d', '--description',
+           metavar='<description>',
+           help='User description of the datanetwork')
+def do_datanetwork_modify(cc, args):
+    """Modify a datanetwork."""
+
+    rwfields = ['mtu', 'description']
+
+    user_specified_fields = dict((k, v) for (k, v) in vars(args).items()
+                                 if k in rwfields and not (v is None))
+
+    patch = []
+    for (k, v) in user_specified_fields.items():
+        patch.append({'op': 'replace', 'path': '/' + k, 'value': v})
+
+    datanetwork = cc.datanetwork.update(args.datanetwork_id, patch)
+    _print_datanetwork_show(datanetwork)
+
+
+@utils.arg('datanetwork_uuid',
+           metavar='<datanetwork_uuid>',
+           help="UUID of datanetwork entry")
+def do_datanetwork_delete(cc, args):
+    """Delete a datanetwork."""
+
+    cc.datanetwork.delete(args.datanetwork_uuid)
+    print('Deleted DataNetwork: %s' % args.datanetwork_uuid)

sysinv/sysinv/sysinv/sysinv/common/exception.py
@@ -1293,3 +1293,57 @@ class LocalManagementIpNotFound(NotFound):
 
 class InvalidHelmDockerImageSource(Invalid):
     message = _("Invalid docker image source: %(source)s. Must be one of %(valid_srcs)s")
+
+
+# DataNetwork
+class UnsupportedInterfaceDataNetworkType(Conflict):
+    message = _("Interface with datanetwork type '%(datanetworktype)s' "
+                "is not supported.")
+
+
+class DataNetworkNotFound(NotFound):
+    message = _("DataNetwork %(datanetwork_uuid)s could not be found.")
+
+
+class DataNetworkTypeNotFound(NotFound):
+    message = _("DataNetwork of type %(network_type)s could not be found.")
+
+
+class DataNetworkIDNotFound(NotFound):
+    message = _("DataNetwork with id %(id)s could not be found.")
+
+
+class DataNetworkNameNotFound(NotFound):
+    message = _("DataNetwork with name %(name)s could not be found.")
+
+
+class DataNetworkAlreadyExists(Conflict):
+    message = _("DataNetwork of name %(name)s already exists.")
+
+
+class DataNetworkTypeUnsupported(Conflict):
+    message = _("DataNetwork of type %(network_type)s is not supported.")
+
+
+class InterfaceDataNetworkNotFound(NotFound):
+    message = _("Interface datanetwork %(uuid)s could not be found.")
+
+
+class InterfaceDataNetworkAlreadyExists(Conflict):
+    message = _("Interface datanetwork with interface ID %(interface_id)s "
+                "and datanetwork ID %(datanetwork_id)s already exists.")
+
+
+class InterfaceDataNetworkNotFoundByKeys(NotFound):
+    message = _("Interface datanetwork with interface ID %(interface_id)s "
+                "and datanetwork ID %(datanetwork_id)s not found")
+
+
+class UnsupportedAssignedInterfaceDataNetworkType(Conflict):
+    message = _("Cannot assign datanetwork with type '%(network_type)s' "
+                "to an interface.")
+
+
+class UnsupportedRemovedInterfaceDataNetworkType(Conflict):
+    message = _("Cannot remove datanetwork with type '%(network_type)s' "
+                "from an interface.")

sysinv/sysinv/sysinv/sysinv/objects/datanetwork.py
@@ -1,0 +1,42 @@
+#
+# Copyright (c) 2019 Wind River Systems, Inc.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+# coding=utf-8
+#
+
+from sysinv.db import api as db_api
+from sysinv.objects import base
+from sysinv.objects import utils
+
+
+class DataNetwork(base.SysinvObject):
+    VERSION = '1.0'
+
+    dbapi = db_api.get_instance()
+
+    fields = {'id': int,
+              'uuid': utils.uuid_or_none,
+              'network_type': utils.str_or_none,
+              'name': utils.str_or_none,
+              'description': utils.str_or_none,
+              'mtu': utils.int_or_none,
+              'multicast_group': utils.str_or_none,
+              'port_num': utils.int_or_none,
+              'ttl': utils.int_or_none,
+              'mode': utils.str_or_none,
+              }
+
+    _optional_fields = {'port_num',
+                        'multicast_group',
+                        'ttl',
+                        'mode'}
+
+    @base.remotable_classmethod
+    def get_by_uuid(cls, context, uuid):
+        return cls.dbapi.datanetwork_get(uuid)
+
+    def save_changes(self, context, updates):
+        self.dbapi.datanetwork_update(self.uuid, updates)

sysinv/sysinv/sysinv/sysinv/puppet/interface.py
@@ -122,13 +122,14 @@ class InterfacePuppet(base.BasePuppet):
             'system_mode': self._get_system().system_mode,
             'ports': self._get_port_interface_id_index(host),
             'interfaces': self._get_interface_name_index(host),
+            'interfaces_datanets': self._get_interface_name_datanets(host),
             'devices': self._get_port_pciaddr_index(host),
             'addresses': self._get_address_interface_name_index(host),
             'routes': self._get_routes_interface_name_index(host),
             'networks': self._get_network_type_index(),
             'gateways': self._get_gateway_index(),
             'floatingips': self._get_floating_ip_index(),
-            'providernets': self._get_provider_networks(host),
+            'datanets': self._get_datanetworks(host),
         }
         return context
 
@@ -160,6 +161,41 @@ class InterfacePuppet(base.BasePuppet):
         interfaces = {}
         for iface in self.dbapi.iinterface_get_by_ihost(host.id):
             interfaces[iface.ifname] = iface
+
+        return interfaces
+
+    def _get_interface_name_datanets(self, host):
+        """
+        Builds a dictionary of datanets indexed by interface name.
+        """
+        interfaces = {}
+        for iface in self.dbapi.iinterface_get_by_ihost(host.id):
+            ifdatanets = self.dbapi.interface_datanetwork_get_by_interface(
+                iface.uuid)
+
+            datanetworks = []
+            for ifdatanet in ifdatanets:
+                datanetworks.append(ifdatanet.datanetwork_uuid)
+
+            datanetworks_list = []
+            for datanetwork in datanetworks:
+                dn = self.dbapi.datanetwork_get(datanetwork)
+                datanetwork_dict = \
+                    {'name': dn.name,
+                     'uuid': dn.uuid,
+                     'network_type': dn.network_type,
+                     'mtu': dn.mtu}
+                if dn.network_type == constants.DATANETWORK_TYPE_VXLAN:
+                    datanetwork_dict.update(
+                        {'multicast_group': dn.multicast_group,
+                         'port_num': dn.port_num,
+                         'ttl': dn.ttl,
+                         'mode': dn.mode})
+                datanetworks_list.append(datanetwork_dict)
+            interfaces[iface.ifname] = datanetworks_list
+
+        LOG.debug("_get_interface_name_datanets ifdatanet=%s" % interfaces)
+
         return interfaces
 
     def _get_port_pciaddr_index(self, host):
@@ -277,17 +313,11 @@ class InterfacePuppet(base.BasePuppet):
 
         return floating_ips
 
-    def _get_provider_networks(self, host):
-        # TODO(alegacy): this will not work as intended for upgrades of AIO-SX
-        # and -DX.  The call to get_providernetworksdict will return an empty
-        # dictionary because the neutron endpoint is not available yet.  Since
-        # we do not currently support SDN/OVS over upgrades we will need to
-        # deal with this in a later commit.
-        pnets = {}
-        if (self.openstack and
-                constants.WORKER in utils.get_personalities(host)):
-            pnets = self.openstack.get_providernetworksdict(quiet=True)
-        return pnets
+    def _get_datanetworks(self, host):
+        dnets = {}
+        if constants.WORKER in utils.get_personalities(host):
+            dnets = self.dbapi.datanetworks_get_all()
+        return dnets
 
 
 def is_platform_network_type(iface):
@@ -461,14 +491,11 @@ def get_interface_mtu(context, iface):
     return iface['imtu']
 
 
-def get_interface_providernets(iface):
+def get_interface_datanets(context, iface):
     """
-    Return the provider networks of the supplied interface as a list.
+    Return the list of data networks of the supplied interface
     """
-    providernetworks = iface['providernetworks']
-    if not providernetworks:
-        return []
-    return [x.strip() for x in providernetworks.split(',')]
+    return context['interfaces_datanets'][iface.ifname]
 
 
 def get_interface_port(context, iface):