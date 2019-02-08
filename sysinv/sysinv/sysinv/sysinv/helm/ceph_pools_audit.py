openstack-archive/stx-config

Add Ceph pools management chart
- new helm chart to set replication and min replication for
each Ceph pool:
    -> new helm chart name: ceph-pools-audit
    -> the ceph-pools-audit chart creates a CronJob that runs
       every 5 minutes; the CronJob checks the replication for
       each existing pool and sets it right if needed
    -> the ceph-pools-audit chart is installed after the
       rbd-provisioner in the application-apply
- new overrides for the ceph-pools-audit chart that provide
the replication values from the attributes of the present
Ceph backends
- enable rados-gw by default when a Ceph backend is enabled

Change-Id: I1565268bac3ddc77e8368d2d6ab8600b3e4ed893
Story: 2004520
Task: 29034
Signed-off-by: Irina Mihai <irina.mihai@windriver.com>

sysinv/sysinv/sysinv/sysinv/helm/ceph_pools_audit.py
@@ -1,0 +1,90 @@
+#
+# Copyright (c) 2019 Wind River Systems, Inc.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from sysinv.common import constants
+from sysinv.common import exception
+from sysinv.openstack.common import log as logging
+
+from sysinv.helm import base
+from sysinv.helm import common
+
+LOG = logging.getLogger(__name__)
+
+
+class CephPoolsAuditHelm(base.BaseHelm):
+    """Class to encapsulate helm operations for the ceph-pools-audit chart"""
+
+    CHART = constants.HELM_CHART_CEPH_POOLS_AUDIT
+    SUPPORTED_NAMESPACES = [
+        common.HELM_NS_OPENSTACK
+    ]
+
+    SERVICE_NAME = 'ceph-pools'
+
+    @property
+    def docker_repo_source(self):
+        return common.DOCKER_SRC_STX
+
+    @property
+    def docker_repo_tag(self):
+        return common.DOCKER_SRCS[self.docker_repo_source][common.IMG_TAG_KEY]
+
+    def get_namespaces(self):
+        return self.SUPPORTED_NAMESPACES
+
+    def get_overrides(self, namespace=None):
+
+        backends = self.dbapi.storage_backend_get_list()
+        ceph_bks = [bk for bk in backends if bk.backend == constants.CINDER_BACKEND_CEPH]
+
+        if not ceph_bks:
+            return {}  # ceph is not configured
+
+        monitors = self._get_formatted_ceph_monitor_ips()
+
+        # Get tier info.
+        tiers = self.dbapi.storage_tier_get_list()
+
+        tiers_cfg = []
+        for bk in ceph_bks:
+            # Get the tier associated to the Ceph backend.
+            tier = next((t for t in tiers if t.forbackendid == bk.id), None)
+            if not tier:
+                raise Exception("No tier present for backend %s" % bk.name)
+
+            # Get the ruleset name.
+            rule_name = "{0}{1}{2}".format(
+                tier.name,
+                constants.CEPH_CRUSH_TIER_SUFFIX,
+                "-ruleset").replace('-', '_')
+
+            # Tier config needed for the overrides.
+            tier_cfg = {
+                    "name": bk.name.encode('utf8', 'strict'),
+                    "replication": int(bk.capabilities.get("replication")),
+                    "min_replication": int(bk.capabilities.get("min_replication")),
+                    "crush_ruleset": rule_name.encode('utf8', 'strict'),
+                  }
+            tiers_cfg.append(tier_cfg)
+
+        overrides = {
+            common.HELM_NS_OPENSTACK: {
+                'conf': {
+                    'ceph': {
+                        'monitors': monitors,
+                        'storage_tiers': tiers_cfg
+                    }
+                }
+            }
+        }
+
+        if namespace in self.SUPPORTED_NAMESPACES:
+            return overrides[namespace]
+        elif namespace:
+            raise exception.InvalidHelmNamespace(chart=self.CHART,
+                                                 namespace=namespace)
+        else:
+            return overrides

starlingx/config

  -> new helm chart name: ceph-pools-audit
  -> the ceph-pools-audit chart creates a CronJob that runs
     every 5 minutes; the CronJob checks the replication for
     each existing pool and sets it right if needed, to reflect
     the attributes of the Ceph backends
  -> the CronJob is needed for: charts that may not manage pool
     configuration, pools created dynamically by services that
     may not have the current pool configuration uploaded
     (ex: swift), updating replication without reinstalling the
     charts that created the pools
  -> the ceph-pools-audit chart is installed after the
     rbd-provisioner in the application-apply

sysinv/sysinv/sysinv/sysinv/helm/ceph_pools_audit.py
@@ -1,0 +1,90 @@
+#
+# Copyright (c) 2019 Wind River Systems, Inc.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from sysinv.common import constants
+from sysinv.common import exception
+from sysinv.openstack.common import log as logging
+
+from sysinv.helm import base
+from sysinv.helm import common
+
+LOG = logging.getLogger(__name__)
+
+
+class CephPoolsAuditHelm(base.BaseHelm):
+    """Class to encapsulate helm operations for the ceph-pools-audit chart"""
+
+    CHART = constants.HELM_CHART_CEPH_POOLS_AUDIT
+    SUPPORTED_NAMESPACES = [
+        common.HELM_NS_OPENSTACK
+    ]
+
+    SERVICE_NAME = 'ceph-pools'
+
+    @property
+    def docker_repo_source(self):
+        return common.DOCKER_SRC_STX
+
+    @property
+    def docker_repo_tag(self):
+        return common.DOCKER_SRCS[self.docker_repo_source][common.IMG_TAG_KEY]
+
+    def get_namespaces(self):
+        return self.SUPPORTED_NAMESPACES
+
+    def get_overrides(self, namespace=None):
+
+        backends = self.dbapi.storage_backend_get_list()
+        ceph_bks = [bk for bk in backends if bk.backend == constants.SB_TYPE_CEPH]
+
+        if not ceph_bks:
+            return {}  # ceph is not configured
+
+        monitors = self._get_formatted_ceph_monitor_ips()
+
+        # Get tier info.
+        tiers = self.dbapi.storage_tier_get_list()
+
+        tiers_cfg = []
+        for bk in ceph_bks:
+            # Get the tier associated to the Ceph backend.
+            tier = next((t for t in tiers if t.forbackendid == bk.id), None)
+            if not tier:
+                raise Exception("No tier present for backend %s" % bk.name)
+
+            # Get the ruleset name.
+            rule_name = "{0}{1}{2}".format(
+                tier.name,
+                constants.CEPH_CRUSH_TIER_SUFFIX,
+                "-ruleset").replace('-', '_')
+
+            # Tier config needed for the overrides.
+            tier_cfg = {
+                    "name": bk.name.encode('utf8', 'strict'),
+                    "replication": int(bk.capabilities.get("replication")),
+                    "min_replication": int(bk.capabilities.get("min_replication")),
+                    "crush_ruleset": rule_name.encode('utf8', 'strict'),
+                  }
+            tiers_cfg.append(tier_cfg)
+
+        overrides = {
+            common.HELM_NS_OPENSTACK: {
+                'conf': {
+                    'ceph': {
+                        'monitors': monitors,
+                        'storage_tiers': tiers_cfg
+                    }
+                }
+            }
+        }
+
+        if namespace in self.SUPPORTED_NAMESPACES:
+            return overrides[namespace]
+        elif namespace:
+            raise exception.InvalidHelmNamespace(chart=self.CHART,
+                                                 namespace=namespace)
+        else:
+            return overrides