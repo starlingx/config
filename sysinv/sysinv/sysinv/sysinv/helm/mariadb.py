c80b8048aed79efbbd82c8a912ee5445cbc2f10b
Chris Friesen <chris.friesen@windriver.com>
2019-04-05 10:34:29 -0400

    Adjust number of mariadb server pods
    For the AIO-DX case (and the dual-controller-with-storage case)
    we now want to use a number of mariadb pods equal to the number of
    controllers.
    
    For the scenarios where there is no garbd daemon running we will
    now have a "dbmon" OCF script that will ensure that the mariadb
    pod on the active controller is part of the primary DB cluster.
    
    (See the "Depends-On" commit messages for more information.)
    
    Depends-On: https://review.openstack.org/650288
    Depends-On: https://review.openstack.org/650301
    
    Change-Id: I9a8ef5555a52bd255f81d87cd26a069e43b4d111
    Story: 2004712
    Task: 30410
    Signed-off-by: Chris Friesen <chris.friesen@windriver.com>

--- sysinv/sysinv/sysinv/sysinv/helm/mariadb.py
+++ sysinv/sysinv/sysinv/sysinv/helm/mariadb.py
@@ -4,7 +4,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-from sysinv.api.controllers.v1 import utils
 from sysinv.common import constants
 from sysinv.common import exception
 from sysinv.openstack.common import log as logging
@@ -20,12 +19,7 @@ class MariadbHelm(openstack.OpenstackBaseHelm):
     CHART = constants.HELM_CHART_MARIADB
 
     def _num_server_replicas(self):
-        # For now we want to run with a single mariadb server pod for the
-        # AIO-DX case.
-        if utils.is_aio_duplex_system(self.dbapi):
-            return 1
-        else:
-            return self._num_controllers()
+        return self._num_controllers()
 
     def get_overrides(self, namespace=None):
         overrides = {
