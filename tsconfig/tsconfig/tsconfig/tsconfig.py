e06a8260c124487aa69a5c3d91ac55c513ce01e8
Wei Zhou <wei.zhou@windriver.com>
2019-04-21 17:52:23 -0400

    Backup and Restore Support for containerized StarlingX
    Define a constant for /opt/platform/helm/<version>
    
    Story: 2004761
    Task: 30522
    Depends-On: I2641c0961f5dff114cb612695b76ac72a80c7554
    Change-Id: I58dc94eaac98a87509f57fdd2126188a6917569c
    signed-off-by: Wei Zhou <wei.zhou@windriver.com>

--- tsconfig/tsconfig/tsconfig/tsconfig.py
+++ tsconfig/tsconfig/tsconfig/tsconfig.py
@@ -171,6 +171,7 @@ PLATFORM_PATH = "/opt/platform"
 CONFIG_PATH = PLATFORM_PATH + "/config/" + SW_VERSION + "/"
 PUPPET_PATH = PLATFORM_PATH + "/puppet/" + SW_VERSION + "/"
 ARMADA_PATH = PLATFORM_PATH + "/armada/" + SW_VERSION
+HELM_OVERRIDES_PATH = PLATFORM_PATH + "/helm/" + SW_VERSION
 CGCS_PATH = "/opt/cgcs"
 KEYRING_PATH = PLATFORM_PATH + "/.keyring/" + SW_VERSION
 EXTENSION_PATH = "/opt/extension"
