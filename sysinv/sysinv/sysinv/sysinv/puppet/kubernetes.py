starlingx/config

commit 5b4ae7503780873552648746f3c9885eb5741014

Enable CPU manager static policy for non-openstack worker nodes
For non-openstack based worker nodes the kubelet cpu-manager
static policy is enabled. The default 'none' policy is retained
for openstack based worker nodes and non-worker nodes.

This enables the complete CPU isolation of:
- Platform tasks (i.e., systemd tasks configured with CPUAffinity)
- kubernetes guaranteed pods (Gu)
- kubernetes besteffort (Be) and burstable (Bu) pods

Refer to following for CPU manager:
https://kubernetes.io/docs/tasks/administer-cluster/cpu-management-policies.

The platform reserved CPUs and memory are queried from sysinv database
for each worker node. Puppet configures the following cpu-manager
options specifically with 'static' policy:
--cpu-manager-policy=static
--system-reserved-cgroup=/system.slice
--system-reserved=cpu=<k8s_reserved_cpus>,memory=<k8s_reserved_mem>Mi

The Allocatable pool of resources is based on the total node capacity
and subtracting overheads including '--system-reserved' and
'--kube-reserved'. Specifying kube-reserved is not necessary.

Change-Id: I235c05b82d50bd9e3a8d36b0b076d8374dfb6055
Story: 2005192
Task: 29952
Signed-off-by: Jim Gauld <james.gauld@windriver.com>

sysinv/sysinv/sysinv/sysinv/puppet/kubernetes.py
@@ -149,12 +149,19 @@ class KubernetesPuppet(base.BasePuppet):
         platform_cpuset = set([c.cpu for c in platform_cpus])
         platform_nodeset = set([c.numa_node for c in platform_cpus])
 
-        # determine set of nonplatform logical cpus and nodes
-        nonplatform_cpuset = host_cpuset - platform_cpuset
-        nonplatform_nodeset = set()
-        for c in host_cpus:
-            if c.cpu not in platform_cpuset:
-                nonplatform_nodeset.update([c.numa_node])
+        # determine platform reserved number of logical cpus
+        k8s_reserved_cpus = len(platform_cpuset)
+
+        # determine platform reserved memory
+        k8s_reserved_mem = 0
+        host_memory = self.dbapi.imemory_get_by_ihost(host.id)
+        numa_memory = utils.get_numa_index_list(host_memory)
+        for node, memory in numa_memory.items():
+            k8s_reserved_mem += memory[0].platform_reserved_mib
+
+        # determine set of nonplatform logical cpus
+        # TODO(jgauld): Commented out for now, using host_cpuset instead.
+        # nonplatform_cpuset = host_cpuset - platform_cpuset
 
         if constants.WORKER in utils.get_personalities(host) \
                 and constants.CONTROLLER not in utils.get_personalities(host):
@@ -162,8 +169,18 @@ class KubernetesPuppet(base.BasePuppet):
                 k8s_cpuset = utils.format_range_set(platform_cpuset)
                 k8s_nodeset = utils.format_range_set(platform_nodeset)
             else:
-                k8s_cpuset = utils.format_range_set(nonplatform_cpuset)
-                k8s_nodeset = utils.format_range_set(nonplatform_nodeset)
+                # kubelet cpumanager is configured with static policy.
+                # The resulting DefaultCPUSet excludes reserved cpus
+                # based on topology, and that also happens to correspond
+                # to the platform_cpuset. kubepods are allowed to
+                # span all host numa nodes.
+                # TODO(jgauld): Temporary workaround until we have a version
+                # of kubelet that excludes reserved cpus from DefaultCPUSet.
+                # The intent is to base k8s_cpuset on nonplatform_cpuset.
+                # Commented out for now, using host_cpuset instead.
+                # k8s_cpuset = utils.format_range_set(nonplatform_cpuset)
+                k8s_cpuset = utils.format_range_set(host_cpuset)
+                k8s_nodeset = utils.format_range_set(host_nodeset)
         else:
             k8s_cpuset = utils.format_range_set(host_cpuset)
             k8s_nodeset = utils.format_range_set(host_nodeset)
@@ -176,6 +193,10 @@ class KubernetesPuppet(base.BasePuppet):
              "\"%s\"" % k8s_cpuset,
              'platform::kubernetes::params::k8s_nodeset':
              "\"%s\"" % k8s_nodeset,
+             'platform::kubernetes::params::k8s_reserved_cpus':
+             k8s_reserved_cpus,
+             'platform::kubernetes::params::k8s_reserved_mem':
+             k8s_reserved_mem,
              })
 
         return config

puppet-manifests/src/modules/platform/manifests/kubernetes.pp
@@ -13,6 +13,9 @@ class platform::kubernetes::params (
   $sa_pub = undef,
   $k8s_cpuset = undef,
   $k8s_nodeset = undef,
+  $k8s_reserved_cpus = undef,
+  $k8s_reserved_mem = undef,
+
 ) { }
 
 class platform::kubernetes::cgroup::params (
@@ -96,6 +99,9 @@ class platform::kubernetes::kubeadm {
   include ::platform::kubernetes::params
 
   $host_labels = $::platform::kubernetes::params::host_labels
+  $k8s_reserved_cpus = $::platform::kubernetes::params::k8s_reserved_cpus
+  $k8s_reserved_mem = $::platform::kubernetes::params::k8s_reserved_mem
+
   $iptables_file = "net.bridge.bridge-nf-call-ip6tables = 1
     net.bridge.bridge-nf-call-iptables = 1"
 
@@ -105,19 +111,36 @@ class platform::kubernetes::kubeadm {
     $k8s_registry = undef
   }
 
-  #only set k8s_hugepage true when subfunction is worker and openstack-compute-node is not in host_labels
+  # Configure kubelet hugepage and cpumanager options
   if str2bool($::is_worker_subfunction)
     and !('openstack-compute-node'
           in $host_labels) {
     $k8s_hugepage = true
+    $k8s_cpu_manager_opts = join([
+      '--cpu-manager-policy=static',
+      '--system-reserved-cgroup=/system.slice',
+      join([
+        '--system-reserved=',
+        "cpu=${k8s_reserved_cpus},",
+        "memory=${k8s_reserved_mem}Mi"])
+      ], ' ')
   } else {
     $k8s_hugepage = false
+    $k8s_cpu_manager_opts = '--cpu-manager-policy=none'
   }
 
-  # enable extra parameters such as hugepage
+  # Enable kubelet extra parameters that are node specific such as
+  # hugepages and cpumanager
   file { '/etc/sysconfig/kubelet':
     ensure  => file,
     content => template('platform/kubelet.conf.erb'),
+  }
+  # The cpu_manager_state file is regenerated when cpumanager starts or
+  # changes allocations so it is safe to remove before kubelet starts.
+  # This file persists so cpumanager's DefaultCPUSet becomes inconsistent
+  # when we offline/online CPUs or change the number of reserved cpus.
+  -> exec { 'remove cpu_manager_state':
+    command => 'rm -f /var/lib/kubelet/cpu_manager_state || true',
   }
 
   # Update iptables config. This is required based on:

puppet-manifests/src/modules/platform/templates/kubelet.conf.erb
@@ -1,3 +1,2 @@
 # Overrides config file for kubelet
-KUBELET_EXTRA_ARGS=--feature-gates=HugePages=<%= @k8s_hugepage %>
-
+KUBELET_EXTRA_ARGS=--feature-gates=HugePages=<%= @k8s_hugepage %> <%= @k8s_cpu_manager_opts %>

commit 2bce834265316ab5226508b020a4b92d76713648

Handle kubernetes reserved memory NoneType exception
The calculation for platform reserved memory is now part of kubernetes
configuration of CPU manager static policy. That code must handle numa
nodes with no platform reserved memory (i.e., value of None).

On a standard configuration, we were getting the following exception:
system host-unlock controller-1
unsupported operand type(s) for +=: 'int' and 'NoneType'

This update fixes the bug introduced by:
https://opendev.org/starlingx/config/commit/5b4ae7503780873552648746f3c9885eb5741014

Closes-Bug: https://bugs.launchpad.net/starlingx/+bug/1828430

Change-Id: I8d277eb1513c40f8f4689f698f8a4932f01017a0
Signed-off-by: Jim Gauld <james.gauld@windriver.com>

sysinv/sysinv/sysinv/sysinv/puppet/kubernetes.py
@@ -157,7 +157,9 @@ class KubernetesPuppet(base.BasePuppet):
         host_memory = self.dbapi.imemory_get_by_ihost(host.id)
         numa_memory = utils.get_numa_index_list(host_memory)
         for node, memory in numa_memory.items():
-            k8s_reserved_mem += memory[0].platform_reserved_mib
+            reserved_mib = memory[0].platform_reserved_mib
+            if reserved_mib is not None:
+                k8s_reserved_mem += reserved_mib
 
         # determine set of nonplatform logical cpus
         # TODO(jgauld): Commented out for now, using host_cpuset instead.