ad38eb8c2977526d9740b799b25fd72eebc06317
Bin Qian <bin.qian@windriver.com>
2019-10-08 15:03:28 -0400

    Remove the limitation of max 4 workers for AIO-DX
    Remove the limitation of max 4 workers for AIO-DX system.
    Rename the function as it has nothing to do with licensing.
    
    Closes-Bug: 1846242
    Change-Id: I34eee9195c48710941e9b1d9eabcf71f794cf874
    Signed-off-by: Bin Qian <bin.qian@windriver.com>

--- sysinv/sysinv/sysinv/sysinv/common/constants.py
+++ sysinv/sysinv/sysinv/sysinv/common/constants.py
@@ -227,9 +227,6 @@ COMBINED_NODE_CONTROLLER_MEMORY_RESERVED_MIB_XEOND = 7000
 # Max number of physical cores in a xeon-d cpu
 NUMBER_CORES_XEOND = 8
 
-# Max number of workers that can be added to an AIO duplex system
-AIO_DUPLEX_MAX_WORKERS = 4
-
 # Network overhead for DHCP or vrouter, assume 100 networks * 40 MB each
 NETWORK_METADATA_OVERHEAD_MIB = 4000
 NETWORK_METADATA_OVERHEAD_MIB_VBOX = 0
