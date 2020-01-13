745aec251eb1247ba75d63465f734e49421db24b
abdelfattah saafan <abdelfattah.saafan@windriver.com>
2020-01-13 09:47:09 -0600

    stx-monitor app: enhanced missing elastic label error message
    When attempting to apply stx-monitor app without the required elastic
    labels, user will get an error message detailing what is missing.
    This label was enhanced to be more readable and provide instructions
    on how to apply the missing labels.
    
    Moved utility methods up to the super class LabelTestCase. Also
    moved host definitions to each of the test classes since
    LabelAssignTestCase require hosts to be locked and
    StxAppLabelsTest require hosts to be unlocked.
    
    Added the following enhanced logic:
    1. All controller nodes are required to have all 4 elastic labels
    2. At least one worker node should have elastic-master label.
    3. Add label value validation and print error message for invalid
    values.
    
    Change-Id: I0b2bc7b87d2e8bf78bfbba455d499c52e1a72349
    Closes-Bug: 1856763
    Signed-off-by: abdelfattah saafan <abdelfattah.saafan@windriver.com>

--- sysinv/sysinv/sysinv/sysinv/tests/api/test_label.py
+++ sysinv/sysinv/sysinv/sysinv/tests/api/test_label.py
@@ -5,9 +5,7 @@
 
 import mock
 import platform
-
 from six.moves import http_client
-
 from sysinv.common import constants
 from sysinv.db import api as dbapi
 from sysinv.tests.api import base
@@ -19,35 +17,25 @@ else:
     from urllib.parse import urlencode
 
 HEADER = {'User-Agent': 'sysinv'}
+es_labels = {'elastic-data': 'enabled',
+                'elastic-controller': 'enabled',
+                'elastic-client': 'enabled',
+                'elastic-master': 'enabled', }
+
+es_worker_labels = {'elastic-master': 'enabled'}
+es_invalid_worker_labels = {'elastic-master': 'mandalorian'}
+
+
+def mock_helm_override_get(dbapi, app_name, chart_name, namespace):
+    return True
 
 
 class LabelTestCase(base.FunctionalTest):
-
     def setUp(self):
         super(LabelTestCase, self).setUp()
         self.dbapi = dbapi.get_instance()
         self.system = dbutils.create_test_isystem()
         self.load = dbutils.create_test_load()
-        self.controller = dbutils.create_test_ihost(
-            id='1',
-            uuid=None,
-            forisystemid=self.system.id,
-            hostname='controller-0',
-            personality=constants.CONTROLLER,
-            subfunctions=constants.CONTROLLER,
-            invprovision=constants.PROVISIONED,
-        )
-        self.worker = dbutils.create_test_ihost(
-            id='2',
-            uuid=None,
-            forisystemid=self.system.id,
-            hostname='worker-0',
-            personality=constants.WORKER,
-            subfunctions=constants.WORKER,
-            mgmt_mac='01:02.03.04.05.C0',
-            mgmt_ip='192.168.24.12',
-            invprovision=constants.PROVISIONED,
-        )
 
     def _get_path(self, host=None, params=None):
         if host:
@@ -58,16 +46,6 @@ class LabelTestCase(base.FunctionalTest):
         if params:
             path += '?' + urlencode(params)
         return path
-
-
-class LabelAssignTestCase(LabelTestCase):
-    def setUp(self):
-        super(LabelAssignTestCase, self).setUp()
-
-    generic_labels = {
-        'apps': 'enabled',
-        'foo': 'bar'
-    }
 
     def validate_labels(self, input_data, response_data):
         self.assertEqual(len(input_data), len(response_data))
@@ -91,6 +69,36 @@ class LabelAssignTestCase(LabelTestCase):
     def get_host_labels(self, host_uuid):
         response = self.get_json("/ihosts/%s/labels" % host_uuid)
         return response['labels']
+
+
+class LabelAssignTestCase(LabelTestCase):
+    def setUp(self):
+        super(LabelAssignTestCase, self).setUp()
+        self.controller = dbutils.create_test_ihost(
+            id='1',
+            uuid=None,
+            forisystemid=self.system.id,
+            hostname='controller-0',
+            personality=constants.CONTROLLER,
+            subfunctions=constants.CONTROLLER,
+            invprovision=constants.PROVISIONED
+        )
+        self.worker = dbutils.create_test_ihost(
+            id='2',
+            uuid=None,
+            forisystemid=self.system.id,
+            hostname='worker-1',
+            personality=constants.WORKER,
+            subfunctions=constants.WORKER,
+            mgmt_mac='01:02:03:04:05:C5',
+            mgmt_ip='192.168.24.14',
+            invprovision=constants.PROVISIONED,
+        )
+
+    generic_labels = {
+        'apps': 'enabled',
+        'foo': 'bar'
+    }
 
     def test_create_labels(self):
         host_uuid = self.worker.uuid
@@ -187,9 +195,7 @@ class LabelAssignTestCase(LabelTestCase):
             host_id=self.worker.id,
             pclass='VGA compatible controller',
             driver='',)
-
         test_plugin_label = {'intelgpu': 'enabled', }
-
         self.assign_labels_failure(self.worker.uuid, test_plugin_label)
 
     @mock.patch('sysinv.api.controllers.v1.label._get_system_enabled_k8s_plugins',
@@ -201,3 +207,135 @@ class LabelAssignTestCase(LabelTestCase):
 
         response_data = self.get_host_labels(self.worker.uuid)
         self.validate_labels(test_plugin_label, response_data)
+
+
+@mock.patch('sysinv.common.utils.is_chart_enabled', mock_helm_override_get)
+class StxAppLabelsTest(LabelTestCase):
+    def setUp(self):
+        super(StxAppLabelsTest, self).setUp()
+        dbutils.create_test_app(name="stx-monitor", status="uploaded", progress="completed")
+        self.controller = dbutils.create_test_ihost(
+            id='1',
+            uuid=None,
+            forisystemid=self.system.id,
+            hostname='controller-0',
+            personality=constants.CONTROLLER,
+            subfunctions=constants.CONTROLLER,
+            invprovision=constants.PROVISIONED,
+            administrative=constants.ADMIN_UNLOCKED,
+            operational=constants.OPERATIONAL_ENABLED
+        )
+        self.controller1 = dbutils.create_test_ihost(
+            id='2',
+            uuid=None,
+            forisystemid=self.system.id,
+            hostname='controller-1',
+            personality=constants.CONTROLLER,
+            subfunctions=constants.CONTROLLER,
+            mgmt_mac='01:02:03:04:05:C1',
+            mgmt_ip='192.168.24.13',
+            invprovision=constants.PROVISIONED,
+            administrative=constants.ADMIN_UNLOCKED,
+            operational=constants.OPERATIONAL_ENABLED
+        )
+        self.worker = dbutils.create_test_ihost(
+            id='3',
+            uuid=None,
+            forisystemid=self.system.id,
+            hostname='worker-0',
+            personality=constants.WORKER,
+            subfunctions=constants.WORKER,
+            mgmt_mac='01:02:03:04:05:C5',
+            mgmt_ip='192.168.24.14',
+            invprovision=constants.PROVISIONED,
+            administrative=constants.ADMIN_UNLOCKED,
+            operational=constants.OPERATIONAL_ENABLED
+        )
+
+    def test_apply_stx_app_with_missing_labels(self):
+        response = self.patch_json('/apps/stx-monitor?directive=apply',
+                        {'values': {}},
+                        headers={'User-Agent': 'sysinv-test'}, expect_errors=True)
+        self.assertIn("system host-label-assign controller-0 elastic-data=enabled",
+                response.json['error_message'])
+        self.assertIn("system host-label-assign controller-1 elastic-data=enabled",
+                response.json['error_message'])
+        self.assertIn("Please use [system host-label-assign <worker hostname> elastic-master=enabled]",
+                response.json['error_message'])
+
+    def test_apply_stx_app_with_missing_worker_labels(self):
+        self.assign_labels(self.controller.uuid, es_labels)
+        response_data_controller0 = self.get_host_labels(self.controller.uuid)
+        self.validate_labels(es_labels, response_data_controller0)
+
+        self.assign_labels(self.controller1.uuid, es_labels)
+        response_data_controller1 = self.get_host_labels(self.controller1.uuid)
+        self.validate_labels(es_labels, response_data_controller1)
+
+        response = self.patch_json('/apps/stx-monitor?directive=apply',
+                        {'values': {}},
+                        headers={'User-Agent': 'sysinv-test'}, expect_errors=True)
+
+        self.assertIn("Please use [system host-label-assign <worker hostname> elastic-master=enabled]",
+                response.json['error_message'])
+        self.assertNotIn("system host-label-assign controller-0 elastic-data=enabled", response.json['error_message'])
+        self.assertNotIn("system host-label-assign controller-1 elastic-data=enabled", response.json['error_message'])
+
+    def test_apply_stx_app_with_missing_controller0_labels(self):
+        self.assign_labels(self.worker.uuid, es_worker_labels)
+        response_data_worker = self.get_host_labels(self.worker.uuid)
+        self.validate_labels(es_worker_labels, response_data_worker)
+
+        self.assign_labels(self.controller1.uuid, es_labels)
+        response_data_controller1 = self.get_host_labels(self.controller1.uuid)
+        self.validate_labels(es_labels, response_data_controller1)
+
+        response = self.patch_json('/apps/stx-monitor?directive=apply',
+                        {'values': {}},
+                        headers={'User-Agent': 'sysinv-test'}, expect_errors=True)
+
+        self.assertIn("system host-label-assign controller-0 elastic-data=enabled", response.json['error_message'])
+        self.assertNotIn("Please use [system host-label-assign <worker hostname> elastic-master=enabled]",
+                response.json['error_message'])
+        self.assertNotIn("system host-label-assign controller-1 elastic-data=enabled", response.json['error_message'])
+
+    def test_apply_stx_app_with_invalid_worker_labels(self):
+        self.assign_labels(self.controller.uuid, es_labels)
+        response_data_controller0 = self.get_host_labels(self.controller.uuid)
+        self.validate_labels(es_labels, response_data_controller0)
+
+        self.assign_labels(self.controller1.uuid, es_labels)
+        response_data_controller1 = self.get_host_labels(self.controller1.uuid)
+        self.validate_labels(es_labels, response_data_controller1)
+
+        self.assign_labels(self.worker.uuid, es_invalid_worker_labels)
+        response_data_worker = self.get_host_labels(self.worker.uuid)
+        self.validate_labels(es_invalid_worker_labels, response_data_worker)
+
+        response = self.patch_json('/apps/stx-monitor?directive=apply',
+                        {'values': {}},
+                        headers={'User-Agent': 'sysinv-test'}, expect_errors=True)
+        self.assertIn("Please use [system host-label-assign <worker hostname> elastic-master=enabled]",
+                response.json['error_message'])
+        self.assertIn("Please correct host labels values to be enabled [worker-0 elastic-master=",
+                response.json['error_message'])
+        self.assertNotIn("system host-label-assign controller-0 elastic-data=enabled", response.json['error_message'])
+        self.assertNotIn("system host-label-assign controller-1 elastic-data=enabled", response.json['error_message'])
+
+    def test_apply_stx_app_with_required_labels(self):
+        self.assign_labels(self.worker.uuid, es_labels)
+        response_data_worker = self.get_host_labels(self.worker.uuid)
+        self.validate_labels(es_labels, response_data_worker)
+
+        self.assign_labels(self.controller.uuid, es_labels)
+        response_data_controller0 = self.get_host_labels(self.controller.uuid)
+        self.validate_labels(es_labels, response_data_controller0)
+
+        self.assign_labels(self.controller1.uuid, es_labels)
+        response_data_controller1 = self.get_host_labels(self.controller1.uuid)
+        self.validate_labels(es_labels, response_data_controller1)
+
+        response = self.patch_json('/apps/stx-monitor?directive=apply',
+                        {'values': {}},
+                        headers={'User-Agent': 'sysinv-test'}, expect_errors=True)
+        self.assertIn("applying", response.json['status'])
