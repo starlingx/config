86aea65f8ea8ad37be1e67f8d305ab10a9142fd3
Steven Webster <steven.webster@windriver.com>
2020-11-03 09:19:20 -0500

    Report port and device inventory after the worker manifest
    Normally, the SR-IOV configuration of a network or device is retained
    across reboots.  In most cases, this is sufficient for the
    sysinv-agent to report port and device inventory at any time after it
    is started.
    
    A problem can occur however for the case of an N3000 FPGA device.
    This device requires a reset on every reboot, which clears the SR-IOV
    configuration until the puppet worker manifest has completed the
    (re)configuration.  In this case, there is a small chance that the
    sysinv-agent audit (every 60 seconds) will run in-between the reset
    and the driver configuration.  Since the agent will only actually
    report the port and device inventory once, a problem can occur
    after a second host lock-unlock, as the SR-IOV configuration data is
    not accurately reflected in the db.
    
    This commit follows a similar method used by the sysinv agent's
    hugepage reporting, in that the port and device configuration are
    only reported after the worker manifest has completed.
    
    Change-Id: Id4af97e0175482d561745a5cb34650dc6b5653b8
    Closes-Bug: 1901968
    Signed-off-by: Steven Webster <steven.webster@windriver.com>

--- sysinv/sysinv/sysinv/sysinv/tests/agent/test_pci.py
+++ sysinv/sysinv/sysinv/sysinv/tests/agent/test_pci.py
@@ -9,10 +9,26 @@ Tests for the sysinv agent pci utilities.
 """
 
 import mock
+try:
+    from contextlib import nested  # Python 2
+except ImportError:
+    from contextlib import ExitStack
+    from contextlib import contextmanager
+
+    @contextmanager
+    def nested(*contexts):
+        """
+        Reimplementation of nested in python 3.
+        """
+        with ExitStack() as stack:
+            yield tuple(stack.enter_context(cm) for cm in contexts)
 
 from sysinv.agent.pci import PCIOperator
+from sysinv.agent.pci import PCI
+from sysinv.agent.manager import AgentManager
 from sysinv.tests import base
 
+import tsconfig.tsconfig as tsc
 
 FAKE_LSPCI_OUTPUT = {
     '82:00.0':
@@ -60,13 +76,49 @@ FAKE_LSPCI_OUTPUT = {
        NUMANode:       1"""
 }
 
+FAKE_DEVICE_ATTRIBUTES = [{
+    "name": "fake_device",
+    "pci_address": "b4:00.0",
+    "pclass_id": "120000",
+    "pvendor_id": "8086",
+    "pdevice_id": "0d8f",
+    "numa_node": 0,
+    "sriov_totalvfs": 8,
+    "sriov_numvfs": 1,
+    "sriov_vfs_pci_address": "b4:00.1",
+    "sriov_vf_driver": "igb_uio",
+    "sriov_vf_pdevice_id": "0d90",
+    "driver": "igb_uio",
+    "enabled": True,
+}]
 
-def mock_get_lspci_output_by_addr(addr):
-    return FAKE_LSPCI_OUTPUT[addr]
+FAKE_PORT_ATTRIBUTES = [{
+    "name": "fake_port",
+    "numa_node": 0,
+    "sriov_totalvfs": 8,
+    "sriov_numvfs": 1,
+    "sriov_vfs_pci_address": "82:10.0",
+    "sriov_vf_driver": "ixgbevf",
+    "sriov_vf_pdevice_id": "000c",
+    "driver": "ixgbe",
+    "pci_address": "82:00.0",
+    "mac": "3c:fd:fe:b5:72:fd",
+    "mtu": 1500,
+    "speed": 1000,
+    "link_mode": 0,
+    "dev_id": 1,
+    "dpdksupport": True
+}]
+
+FAKE_PORTS = [PCI(
+    '82:10.0', 'Ethernet controller [0200]', 'Intel Corporation [8086]', 'Device [1518]',
+    '-r04', 'Intel Corporation [8086]', 'Device [0000]')]
+
+FAKE_DEVICES = [PCI(
+    'b4:00.0', 'Processing accelerators', 'Intel Corporation [8086]', 'Device [0d8f]',
+    '', 'Intel Corporation [8086]', 'Device [0000]')]
 
 
-@mock.patch.object(PCIOperator, 'get_lspci_output_by_addr',
-                   side_effect=mock_get_lspci_output_by_addr)
 class TestPciOperator(base.TestCase):
 
     def setUp(self):
@@ -76,20 +128,112 @@ class TestPciOperator(base.TestCase):
     def tearDown(self):
         super(TestPciOperator, self).tearDown()
 
+    def mock_get_lspci_output_by_addr(addr):
+        return FAKE_LSPCI_OUTPUT[addr]
+
+    @mock.patch.object(PCIOperator, 'get_lspci_output_by_addr',
+                       side_effect=mock_get_lspci_output_by_addr)
     def test_get_pci_sriov_vf_driver_name(self, get_lspci_output_by_addr):
         pfaddr = '82:00.0'
         vfaddrs = ['82:10.0']
         result = self.pci_operator.get_pci_sriov_vf_driver_name(pfaddr, vfaddrs)
         assert result == "vfio-pci"
 
+    @mock.patch.object(PCIOperator, 'get_lspci_output_by_addr',
+                       side_effect=mock_get_lspci_output_by_addr)
     def test_get_pci_sriov_vf_module_name(self, get_lspci_output_by_addr):
         pfaddr = '82:00.0'
         vfaddrs = ['82:10.0']
         result = self.pci_operator.get_pci_sriov_vf_module_name(pfaddr, vfaddrs)
         assert result == "ixgbevf"
 
+    @mock.patch.object(PCIOperator, 'get_lspci_output_by_addr',
+                       side_effect=mock_get_lspci_output_by_addr)
     def test_get_pci_sriov_vf_module_name_none(self, get_lspci_output_by_addr):
         pfaddr = 'b4:00.0'
         vfaddrs = ['b4:00.1']
         result = self.pci_operator.get_pci_sriov_vf_module_name(pfaddr, vfaddrs)
         assert result is None
+
+
+class TestAgentOperator(base.TestCase):
+
+    def setUp(self):
+        super(TestAgentOperator, self).setUp()
+        self.agent_manager = AgentManager('test-host', 'test-topic')
+
+    def tearDown(self):
+        super(TestAgentOperator, self).tearDown()
+
+    def _get_ports_inventory(self):
+        with nested(mock.patch.object(PCIOperator, 'pci_get_net_attrs'),
+                    mock.patch.object(PCIOperator, 'pci_get_device_attrs'),
+                    mock.patch.object(PCIOperator, 'inics_get'),
+                    mock.patch.object(PCIOperator, 'pci_devices_get'),
+                    mock.patch.object(AgentManager, '_acquire_network_config_lock'),
+                    mock.patch.object(AgentManager, '_release_network_config_lock')) as (
+                        mock_net_attrs, mock_device_attrs, mock_nics, mock_devices,
+                        aquire_lock, release_lock):
+
+            def fake_get_net_attrs(a):
+                return FAKE_PORT_ATTRIBUTES
+
+            def fake_get_device_attrs(a):
+                return FAKE_DEVICE_ATTRIBUTES
+
+            def fake_get_nics():
+                return FAKE_PORTS
+
+            def fake_get_devices():
+                return FAKE_DEVICES
+
+            mock_net_attrs.side_effect = fake_get_net_attrs
+            mock_device_attrs.side_effect = fake_get_device_attrs
+            mock_nics.side_effect = fake_get_nics
+            mock_devices.side_effect = fake_get_devices
+
+            ports, devices, macs = self.agent_manager._get_ports_inventory()
+            return ports, devices, macs
+
+    @mock.patch('os.path.exists')
+    def test_get_pci_inventory_before_worker_initial_config_complete(self, mock_exists):
+        def file_exists_side_effect(filename):
+            # Neither the initial nor volatile worker config complete flags are set
+            return False
+        mock_exists.side_effect = file_exists_side_effect
+
+        ports, devices, macs = self._get_ports_inventory()
+        assert len(ports) == 1
+        assert len(devices) == 1
+        assert len(macs) == 1
+
+    @mock.patch('os.path.exists')
+    def test_get_pci_inventory_before_worker_config_complete(self, mock_exists):
+        def file_exists_side_effect(filename):
+            if filename == tsc.INITIAL_WORKER_CONFIG_COMPLETE:
+                # Only the initial worker config complete flag is set
+                return True
+            else:
+                return False
+        mock_exists.side_effect = file_exists_side_effect
+
+        ports, devices, macs = self._get_ports_inventory()
+        assert len(ports) == 0
+        assert len(devices) == 0
+        assert len(macs) == 0
+
+    @mock.patch('os.path.exists')
+    def test_get_pci_inventory_after_worker_config_complete(self, mock_exists):
+        def file_exists_side_effect(filename):
+            if filename in [tsc.INITIAL_WORKER_CONFIG_COMPLETE,
+                            tsc.VOLATILE_WORKER_CONFIG_COMPLETE]:
+                # Both of the initial and volatile worker config complete flags are set
+                return True
+            else:
+                return False
+        mock_exists.side_effect = file_exists_side_effect
+
+        ports, devices, macs = self._get_ports_inventory()
+        assert len(ports) == 1
+        assert len(devices) == 1
+        assert len(macs) == 1
