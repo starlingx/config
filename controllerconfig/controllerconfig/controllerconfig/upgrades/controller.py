84936cf189215ca60b9210ce27783f6331a98679
Zhipeng Liu <zhipengs.liu@intel.com>
2021-01-11 22:53:55 +0800

    Fix etcd AIO-DX upgrade failure issue
    The data migration is failed after controller-1 is upgraded
    (system host-upgrade controller-1) which is due to the error
    “/opt/platform/puppet/20.12/hieradata/static.yaml doesn’t exist.”
    when migrating the hieradata.
    
    Closes-Bug: 1894870
    
    Change-Id: I71a5ef6d43da13039d0607b2457bd5f561704dfe
    Signed-off-by: Zhipeng Liu <zhipengs.liu@intel.com>

--- controllerconfig/controllerconfig/controllerconfig/upgrades/controller.py
+++ controllerconfig/controllerconfig/controllerconfig/upgrades/controller.py
@@ -720,17 +720,19 @@ def migrate_hiera_data(from_release, to_release, role=None):
     # apply-bootstrap-manifest. Need to get it and update to target
     # static.yaml.
     static_file = os.path.join(to_hiera_path, "static.yaml")
-    with open(static_file, 'r') as yaml_file:
-        static_config = yaml.load(yaml_file)
-
     etcd_security_config = {}
-    if 'platform::etcd::params::security_enabled' in static_config.keys():
-        etcd_security_config['platform::etcd::params::security_enabled'] = \
-            static_config['platform::etcd::params::security_enabled']
-        etcd_security_config['platform::etcd::params::bind_address'] = \
-            static_config['platform::etcd::params::bind_address']
-        etcd_security_config['platform::etcd::params::bind_address_version'] = \
-            static_config['platform::etcd::params::bind_address_version']
+
+    if os.path.exists(static_file):
+        with open(static_file, 'r') as yaml_file:
+            static_config = yaml.load(yaml_file)
+
+        if 'platform::etcd::params::security_enabled' in static_config.keys():
+            etcd_security_config['platform::etcd::params::security_enabled'] = \
+                static_config['platform::etcd::params::security_enabled']
+            etcd_security_config['platform::etcd::params::bind_address'] = \
+                static_config['platform::etcd::params::bind_address']
+            etcd_security_config['platform::etcd::params::bind_address_version'] = \
+                static_config['platform::etcd::params::bind_address_version']
 
     shutil.rmtree(to_hiera_path, ignore_errors=True)
     os.makedirs(to_hiera_path)
