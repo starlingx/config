ace7088e7ee088d840e305b88801c5e76a9a5796
Andy Ning <andy.ning@windriver.com>
2022-10-05 17:03:30 -0400

    Update upgrade migration scripts for openldap
    This change removed 66-create-open-ldap-certificate.py "activate"
    script since all openldap certificates are generated during host
    unlock.
    
    A new migration script is added to update openldap users carried
    over from the from-release side. This script will run at "activate"
    phase of upgrade.
    
    Test Plan:
    PASS: system upgrade from stx r6 to r7.
    PASS: Verify client access to secure openldap by
          ldapsearch -xH ldaps://controller -b "ou=People,dc=cgcs,dc=local"
    PASS: Verify client access to secure openldap by
          ldapfinger admin
    PASS: Verify openldap user login from console and by ssh.
    
    Story: 2009834
    Task: 46517
    Signed-off-by: Andy Ning <andy.ning@windriver.com>
    Change-Id: I8b72d8ea1db4052fc09250010fb6c2beefdf014c

---
+++ controllerconfig/controllerconfig/upgrade-scripts/67-update-openldap-users.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#
+# Copyright (c) 2022 Wind River Systems, Inc.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+#
+# This migration script is used for update openldap users during the
+# activate stage of a platform upgrade. It will:
+# - change admin user's primary group from 'root' to 'users'
+
+# The migration scripts are passed these parameters:
+NAME=$(basename $0)
+FROM_RELEASE=$1
+TO_RELEASE=$2
+ACTION=$3
+
+# This will log to /var/log/platform.log
+function log {
+    logger -p local1.info $1
+}
+
+# Script start
+log "$NAME: Starting updating openldap users from release $FROM_RELEASE to $TO_RELEASE with action $ACTION"
+
+if [[ "${ACTION}" == "activate" ]] && [[ "${TO_RELEASE}" == "22.12" ]]; then
+
+    /usr/sbin/ldapsetprimarygroup admin users
+
+    RC=$?
+    if [ ${RC} -eq 0 ]; then
+        log "$NAME: Successfully updated openldap users."
+    else
+        log "$NAME: ERROR - failed to update openldap users. (RETURNED: $RC)"
+        exit 1
+    fi
+else
+    log "$NAME: No actions required for from release $FROM_RELEASE to $TO_RELEASE with action $ACTION"
+fi
+
+exit 0
+
