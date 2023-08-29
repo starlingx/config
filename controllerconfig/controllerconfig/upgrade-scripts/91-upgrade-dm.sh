d7b30649eeba379aa8f74c54631d22e56d834540
Sriram Narasimhan <sriram.narasimhan@windriver.com>
2023-08-29 08:10:59 -0400

    Refresh deploy plug-in only when the plug-in is present
    This commit will ensure that deploy plug-in is refreshed
    only when it is installed in the first place.
    
    Test Plan:
    PASS: Verify the deploy plug-in is refreshed when it
          is installed
    PASS: Verify the script execution is skipped when
          deploy plug-in is not installed
    
    Story: 2010718
    Task: 48688
    
    Change-Id: I3b85dca9207ed1222cdbe61c5fee2789e41a1b88
    Signed-off-by: Sriram Narasimhan <sriram.narasimhan@windriver.com>

--- controllerconfig/controllerconfig/upgrade-scripts/91-upgrade-dm.sh
+++ controllerconfig/controllerconfig/upgrade-scripts/91-upgrade-dm.sh
@@ -24,12 +24,20 @@ DEPLOY_OVERRIDES=$(ls /usr/local/share/applications/overrides/*deployment-manage
 REFRESH_DM_IMAGES="false"
 
 if [[ "${ACTION}" == "activate" ]]; then
-    if [[ -z "${DEPLOY_OVERRIDES}" ]] || [[ -z "${DEPLOY_PLAYBOOK}" ]] || [[ -z "${DEPLOY_CHART}" ]]; then
-        log "$NAME: script execution is skipped. There are no deploy files."
+    if kubectl --kubeconfig=/etc/kubernetes/admin.conf get namespace| grep -q deployment-manager
+    then
+        if [[ -z "${DEPLOY_OVERRIDES}" ]] || [[ -z "${DEPLOY_PLAYBOOK}" ]] || [[ -z "${DEPLOY_CHART}" ]]; then
+            log "$NAME: Script execution is skipped. There are no deploy files."
+        else
+            log "$NAME: Refreshing deploy plug-in from $FROM_RELEASE to $TO_RELEASE"
+            /usr/local/bin/update-dm.sh ${DEPLOY_PLAYBOOK} \
+                                        ${DEPLOY_CHART} \
+                                        ${DEPLOY_OVERRIDES} \
+                                        ${REFRESH_DM_IMAGES}
+            exit $?
+        fi
     else
-        log "$NAME: Refreshing deploy plug-in from $FROM_RELEASE to $TO_RELEASE"
-        /usr/local/bin/update-dm.sh ${DEPLOY_PLAYBOOK} ${DEPLOY_CHART} ${DEPLOY_OVERRIDES} ${REFRESH_DM_IMAGES}
-        exit $?
+        log "$NAME: Script execution is skipped. There is no deploy plug-in running in ${FROM_RELEASE}."
     fi
 else
     log "$NAME: No actions required for from release $FROM_RELEASE to $TO_RELEASE with action $ACTION"
