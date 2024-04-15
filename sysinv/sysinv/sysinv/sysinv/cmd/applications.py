cecc1bb42194179adb08cc9df353f92125a4bd1c
Joshua Reed <joshua.reed@windriver.com>
2024-04-15 13:58:40 -0600

    Provide helper CLI utility to check app K8s compatibility.
    Add a CLI tool which will allow a developer to learn which
    apps are compatible between the current K8s on the platform
    and the target version specified.  There are also safeguards
    to prevent the user from supplying an invalid target K8s
    version.
    
    Add an --include-path option to provide the full file path
    instead of simply the app name in the output
    
    Usage:
    
    sysinv-app query <target-k8s-version> [Optional: --include-path]
    
    Output:
    
    app_name_1
    app_name_2
    
    Output with --include-path:
    /some/path/app_name_1.ver.tgz
    /another/path/app_name_2.var.tgz
    
    Depends-On: https://review.opendev.org/c/starlingx/config/+/909172
    
    Test Plan:
    PASS: Vary the current K8S installed on the platform
          and manually modify the minimum/maximum K8s version
          for an app in the KubAppBundle table. Verify that
          the correct app list prints to terminal.
    
    Story: 2010929
    Task: 49875
    
    Change-Id: Ie8dfb8cff9a587f9b52f8d94f7dd089c46dd5d63
    Signed-off-by: Joshua Reed <joshua.reed@windriver.com>

--- sysinv/sysinv/sysinv/sysinv/cmd/applications.py
+++ sysinv/sysinv/sysinv/sysinv/cmd/applications.py
@@ -10,13 +10,20 @@ System Inventory Application Verification & Validation Utility.
 
 import sys
 
+from distutils.version import LooseVersion
 from oslo_config import cfg
+from oslo_log import log
 
+from sysinv.common import exception
+from sysinv.common import kubernetes
 from sysinv.common import service
 from sysinv.common.app_metadata import verify_application
 from sysinv.common.app_metadata import verify_application_tarball
+from sysinv.db import api
 
 CONF = cfg.CONF
+
+LOG = log.getLogger(__name__)
 
 
 def verify_application_tox(path):
@@ -37,6 +44,94 @@ def verify_application_metadata(path):
     verify_application_tarball(path)
 
 
+def make_application_query(k8s_ver, include_path=False):
+    """Query which applications are compatible.
+
+    From the current K8S to the target K8S version
+    specified, print out a list of compatible apps
+    to terminal.
+
+    Args:
+        k8s_ver: String kubernetes version.
+        include_path: (Optional) if True, then the output
+        will use the full tgz path to specify compatible
+        app instead of just the name.
+    """
+    # K8S Version
+    target_version = k8s_ver[0]
+
+    # Cleanup input:
+    if 'v' in target_version:
+        target_version = target_version.lstrip('v').strip()
+
+    dbapi = api.get_instance()
+    k8s_operator = kubernetes.KubeOperator()
+
+    # Get current kubernetes version of the platform
+    try:
+        version = k8s_operator.kube_get_kubernetes_version()
+        version = version.lstrip('v')  # Remove leading 'v'
+    except Exception:
+        # During initial installation of the first controller,
+        # kubernetes may not be running yet. In that case, none of the
+        # puppet manifests being applied will need the kubernetes
+        # version.
+        LOG.warning("Unable to retrieve kubernetes version")
+
+    # All installed K8S Apps.
+    try:
+        db_apps = dbapi.kube_app_get_all()
+    except exception.KubeAppNotFound:
+        LOG.info("Unable to obtain K8s app list.")
+        return
+
+    # Check to see if the target version is valid first.
+    supported_versions = \
+        [x['version'].lstrip('v') for x in kubernetes.get_kube_versions()]
+
+    if target_version not in supported_versions:
+        print("Error: The supplied version is not supported. Exiting...")
+        sys.exit(1)
+
+    # If target_version is less than current version, throw an error and exit.
+    if LooseVersion(target_version) < LooseVersion(version):
+        print(
+            "Error: Target version cannot be lower than the current version."
+            " Exiting..."
+        )
+        sys.exit(1)
+
+    # Now check all apps to see which are compatible.
+    compatible_apps = []
+    for app in db_apps:
+        app_name = app.name
+        bundle = dbapi.kube_app_bundle_get_all(name=app_name)
+
+        if len(bundle) > 1:
+            print(
+                "Error: App: {app_name} Should only have 1 bundle object! "
+                "Exiting..."
+            )
+            sys.exit(1)
+
+        bundle = bundle[0]
+        update_compatible = dbapi.kube_app_bundle_is_k8s_compatible(
+            app_name,
+            bundle.k8s_timing,
+            target_version,
+            current_k8s_version=version
+        )
+
+        if update_compatible:
+            if include_path:
+                compatible_apps.append(bundle.file_path)
+            else:
+                compatible_apps.append(app_name)
+
+    for app in compatible_apps:
+        print(app)
+
+
 def add_action_parsers(subparsers):
     parser = subparsers.add_parser('verify-metadata')
     parser.set_defaults(func=verify_application_metadata)
@@ -45,6 +140,11 @@ def add_action_parsers(subparsers):
     parser = subparsers.add_parser('tox')
     parser.set_defaults(func=verify_application_tox)
     parser.add_argument('path', nargs='?')
+
+    parser = subparsers.add_parser('query')
+    parser.set_defaults(func=make_application_query)
+    parser.add_argument('k8s_ver', nargs=1)
+    parser.add_argument('--include-path', action='store_true', default=False)
 
 
 CONF.register_cli_opt(
@@ -56,4 +156,10 @@ CONF.register_cli_opt(
 
 def main():
     service.prepare_service(sys.argv)
-    CONF.action.func(CONF.action.path)
+    if CONF.action.name == "tox" or CONF.action.name == "verify-metadata":
+        CONF.action.func(CONF.action.path)
+    elif CONF.action.name == "query":
+        include_path = True if '--include-path' in sys.argv else False
+        CONF.action.func(CONF.action.k8s_ver, include_path=include_path)
+    else:
+        print(f"Unsupported action verb: {CONF.action.name}")
