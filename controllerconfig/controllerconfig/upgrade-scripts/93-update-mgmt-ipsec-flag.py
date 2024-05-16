776d4647e4512dc8d5e8115b580f03f7eb4e9bc9
Heitor Matsui <heitorvieira.matsui@windriver.com>
2024-05-16 11:33:07 -0300

    Fix parameter in upgrade script
    Script introduced by [1] broke USM deploy start due to not
    parsing the fourth parameter passed to the scripts during
    the data migration.
    
    This commit fixes the issue.
    
    [1] https://review.opendev.org/c/starlingx/config/+/919624
    
    Test Plan
    PASS: execute deploy start successfully
    
    Closes-bug: 2065910
    
    Change-Id: I03a9e055bc487b423c385ee7fb69fc31396ac734
    Signed-off-by: Heitor Matsui <heitorvieira.matsui@windriver.com>

--- controllerconfig/controllerconfig/upgrade-scripts/93-update-mgmt-ipsec-flag.py
+++ controllerconfig/controllerconfig/upgrade-scripts/93-update-mgmt-ipsec-flag.py
@@ -33,6 +33,10 @@ def main():
             to_release = sys.argv[arg]  # noqa
         elif arg == 3:
             action = sys.argv[arg]
+        elif arg == 4:
+            # optional port parameter for USM upgrade
+            # port = sys.argv[arg]
+            pass
         else:
             print(f"Invalid option {sys.argv[arg]}.")
             return 1
