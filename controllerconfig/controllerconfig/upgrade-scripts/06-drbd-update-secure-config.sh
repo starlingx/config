1c22dac92a829c33250901c441ad85a729da27d9
Guilherme Costa <guilherme.costa@windriver.com>
2024-09-04 21:57:39 -0300

    Adding retries for drbdsync-modify command
    During upgrade there is a possibility to the API to take longer to
    process drbdsync-modify request. Adding more retries here could
    solve the problem.
    
    Test Plan:
    PASS: SX upgrade finishes successfully. Confirm service
          parameter modify/delete properly with CLI
          system service-parameter-[modify|delete] and confirm
          hieradata and drbdconfig has expected values and drbd
          stays working properly
    PASS: DX upgrade finishes successfully. Confirm service
          parameter modify/delete properly with CLI
          system service-parameter-[modify|delete] and confirm
          hieradata and drbdconfig has expected values and drbd
          stays working properly
    
    Story: 2011139
    Task: 50991
    
    Change-Id: I1013927fea4f175c9775d70ad6f6316e6fccd1ea
    Signed-off-by: Guilherme Costa <guilherme.costa@windriver.com>

--- controllerconfig/controllerconfig/upgrade-scripts/06-drbd-update-secure-config.sh
+++ controllerconfig/controllerconfig/upgrade-scripts/06-drbd-update-secure-config.sh
@@ -31,19 +31,73 @@ FROM_RELEASE=$1
 TO_RELEASE=$2
 ACTION=$3
 
+RETRY_INTERVAL=10
+RETRY_CNT=10
+
 # This will log to /var/log/platform.log
 function log {
     logger -p local1.info $1
 }
 
-log "$NAME: Starting drbdconfig secure toggle from release $FROM_RELEASE to $TO_RELEASE with action $ACTION"
+function get_api_token {
+    curl -v -X POST "${1}/auth/tokens" \
+    --header 'Content-Type: application/json' \
+    --data '{
+        "auth": {
+            "identity": {
+                "methods": [
+                    "password"
+                ],
+                "password": {
+                    "user": {
+                        "domain": {
+                            "name": "Default"
+                        },
+                        "name": "'${2}'",
+                        "password": "'${3}'"
+                    }
+                }
+            },
+            "scope": {
+                "project": {
+                    "domain": {
+                        "name": "Default"
+                    },
+                    "name": "admin"
+                }
+            }
+        }
+    }' 2>&1 | sed -n 's/.*[t|T]oken: \(.*\)/\1/p'
+}
+
+log "${NAME}: Starting drbdconfig secure toggle from release ${FROM_RELEASE} to ${TO_RELEASE} with action ${ACTION}"
 
 if [[ "${ACTION}" == "activate" ]] && [[ "${TO_RELEASE}" == "24.09" ]]; then
     source /etc/platform/openrc
-    if system drbdsync-modify --secure True; then
-        log "$NAME: drbdconfig secure toggle is True."
-    else
-        log "$NAME: drbdconfig secure toggle was not possible."
-        exit 1
-    fi
+
+    while [ ${RETRY_CNT} -ge 0 ];
+    do
+        TOKEN=$(get_api_token "${OS_AUTH_URL}" "${OS_USERNAME}" "${OS_PASSWORD}")
+        PARAMETERS_JSON=$(curl -s -X GET "http://controller:6385/v1/service_parameter/" -H "X-Auth-Token: ${TOKEN}")
+        DRBDSYNC_SECURE=$(echo "${PARAMETERS_JSON}" | sed -n 's/.*"section": "drbd", "name": "secure", "value": "\([^"]*\)".*/\1/p')
+
+        if [[ "${DRBDSYNC_SECURE}"  == 'True' ]]; then
+            log "${NAME}: drbdconfig secure toggle is True."
+            exit 0
+        fi
+
+        if  system drbdsync-modify --secure True; then
+            log "${NAME}: drbdconfig secure toggle is True."
+            exit 0
+        fi
+
+        if [ ${RETRY_CNT} -gt 0 ]; then
+            log "${NAME}: retrying drbdconfig secure toggle ( remaining ${RETRY_CNT} )."
+            RETRY_CNT=$((RETRY_CNT-1))
+            sleep ${RETRY_INTERVAL}
+        else
+            log "${NAME}: drbdconfig secure toggle was not possible."
+            exit 1
+        fi
+    done
 fi
