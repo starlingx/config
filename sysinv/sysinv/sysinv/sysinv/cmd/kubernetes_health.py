addb42cfa21c8ed840fea806028f5dbb3f7daa59
Igor Soares <Igor.PiresSoares@windriver.com>
2024-09-13 19:38:30 -0300

    Command-line application for checking k8s health
    Add a sysinv command-line application for checking Kubernetes health.
    The ability for checking all Kubernetes endpoints in parallel was added
    leveraging the existing health check available within the sysinv code
    base.
    
    Usage: sysinv-k8s-health check [-h] [tries] [try_sleep] [timeout]
    
    Positional arguments:
      tries:     Maximum number of retries to check endpoints health.
                 (Default: 20)
      try_sleep: Sleep interval between retries (in seconds).
                 (Default: 5s)
      timeout:   Time which wait on a response before timing out (in
                 seconds). (Default: 5s)
    
    Return codes: 0 if all endpoints are healthy
                  1 if at least one endpoint is unhealthy
    
    Endpoints tested:
      - API server: https://localhost:6443/readyz
      - Scheduler: https://127.0.0.1:10259/healthz
      - Controller: https://127.0.0.1:10257/healthz
      - Kubelet: http://localhost:10248/healthz
    
    Test plan:
    PASS: build-pkgs && build-image
    PASS: Run "sysinv-k8s-health check" against a healthy Kubernetes
          instance.
    PASS: Block ports 6443, 10257, 10259 and 10248 via iptables to simulate
          an unhealthy Kubernetes instance.
          Run "sysinv-k8s-health check"
    PASS: Test different combinations of "tries", "try_sleep" and "timeout"
          parameters.
    
    Partial-bug: 2080863
    
    Change-Id: Ifa40b2ea6ba13e179f498ce31d9da069da4a8d8c
    Signed-off-by: Igor Soares <Igor.PiresSoares@windriver.com>

---
+++ sysinv/sysinv/sysinv/sysinv/cmd/kubernetes_health.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+#
+# Copyright (c) 2024 Wind River Systems, Inc.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+"""
+System Inventory Kubernetes Health Utility
+"""
+
+import sys
+
+from oslo_config import cfg
+from oslo_log import log
+
+from sysinv.common import service
+from sysinv.common.kubernetes import k8s_wait_for_endpoints_health
+
+CONF = cfg.CONF
+LOG = log.getLogger(__name__)
+SUPPORTED_ACTIONS = ['check']
+
+
+def add_action_parsers(subparsers):
+    parser = subparsers.add_parser('check')
+    parser.set_defaults(func=k8s_wait_for_endpoints_health)
+    parser.add_argument('tries', type=int, nargs='?', default=20)
+    parser.add_argument('try_sleep', type=int, nargs='?', default=5)
+    parser.add_argument('timeout', type=int, nargs='?', default=5)
+
+
+CONF.register_cli_opt(
+    cfg.SubCommandOpt('action',
+                      title='actions',
+                      help='Check Kubernetes health',
+                      handler=add_action_parsers))
+
+
+def main():
+    service.prepare_service(sys.argv)
+    if CONF.action.name in SUPPORTED_ACTIONS:
+        LOG.info("Checking Kubernetes health...")
+        if CONF.action.func(CONF.action.tries, CONF.action.try_sleep, CONF.action.timeout):
+            exit(0)
+    else:
+        print(f"Unsupported action: {CONF.action.name}. Supported actions: {SUPPORTED_ACTIONS}",
+              file=sys.stderr)
+
+    exit(1)
