d29277255fb5b14c0b00d197a94b41ab9d9d52e3
Boovan Rajendran <boovan.rajendran@windriver.com>
2024-10-09 11:53:22 -0400

    Workaround: Remove 500.200 Alarm for cdi-uploadserver-client-cert
    After uploading and applying the KubeVirt app, a 500.200 alarm is
    triggered for the cdi-uploadserver-client-cert certificate.
    This is a known issue, discussed on GitHub
    https://github.com/kubevirt/containerized-data-importer/issues/2755
    and has been fixed in CDI version 1.60.
    
    This change serves as a workaround to suppress the alarm for the
    cdi-uploadserver-client-cert certificate until we upgrade to
    CDI version 1.60. This enable the subsequent Kubernetes upgrade
    to initiateand finish successfully.
    Using Kubernetes watch to continuously monitor the cdi in a separate
    thread
    
    Test Plan:
    PASS: Upload and apply the kubevirt-app, then verify that the
    500.200 alarm for cdi-uploadserver-client-cert does not appear
    after running the health status check.
    PASS: Perform single and Multi k8s upgrade.
    
    Partial-Bug: 2084142
    
    Change-Id: I1e17950bcdfc421a1424120e2ea5261623e9204c
    Signed-off-by: Boovan Rajendran <boovan.rajendran@windriver.com>

---
+++ sysinv/sysinv/sysinv/sysinv/cert_alarm/cdi_watch.py
@@ -0,0 +1,87 @@
+#
+# Copyright (c) 2024 Wind River Systems, Inc.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+import eventlet
+from kubernetes import client
+from kubernetes import config
+from kubernetes import watch
+import logging
+from sysinv.common import kubernetes as sys_kube
+
+# Constants for CDI resources
+CDI_GROUP = 'cdi.kubevirt.io'
+CDI_VERSION = 'v1beta1'
+CDI_PLURAL = 'cdis'
+
+LOG = logging.getLogger(__name__)
+
+
+class CDIWatch(object):
+    def __init__(self):
+        self.current_version = None
+        self.watch_thread = None
+        self.watch_event = eventlet.event.Event()
+        self.watch_cdi = watch.Watch()
+        self.monitor_thread = False
+        self.ccObj = None
+
+    def set_version(self, version):
+        # Set the current version of the CDI resource
+        self.current_version = version
+
+    def get_version(self):
+        # Get the current version of the CDI resource
+        return self.current_version
+
+    def is_monitor_thread_started(self):
+        return self.monitor_thread
+
+    def start_watching(self):
+        config.load_kube_config(sys_kube.KUBERNETES_ADMIN_CONF)
+        self.ccObj = client.CustomObjectsApi()
+        if self.watch_thread and not self.watch_thread.dead:
+            LOG.info("Thread is already running.")
+            return
+        self.watch_thread = eventlet.greenthread.spawn(self.watch_cdi_resources, self.watch_event)
+        self.monitor_thread = True
+        self.watch_event.wait()
+        self.watch_event.reset()
+
+    def __stop_watching(self, stop_event):
+        # Set the stop event to signal the thread to stop
+        if self.watch_thread:
+            if not stop_event.ready():
+                stop_event.send()
+            self.current_version = None
+            self.monitor_thread = False
+            self.watch_thread.kill()
+            self.watch_thread.wait()
+
+    def watch_cdi_resources(self, watch_event):
+        '''
+        Streams events for CDI resources and raises an exception if streaming fails.
+
+        This method continuously monitors changes to CDI resources.
+        It updates the current operator version on 'ADDED' or 'MODIFIED' events
+        and signals through the provided watch_event.
+        Raises:
+            Exception: If an error occurs while streaming events.
+        '''
+        try:
+            # Stream events for CDI resources
+            for event in self.watch_cdi.stream(self.ccObj.list_cluster_custom_object,
+                                          group=CDI_GROUP, version=CDI_VERSION, plural=CDI_PLURAL):
+                event_type = event['type']
+                if event_type in ['ADDED', 'MODIFIED']:
+                    resource = event['object']
+                    current_version = resource.get('status', {}).get('operatorVersion')
+                    self.set_version(current_version)
+                    if not watch_event.ready():
+                        watch_event.send()
+        except Exception as e:
+            msg = "monitor action failed: %s" % str(e)
+            LOG.error(msg)
+            self.__stop_watching(watch_event)
