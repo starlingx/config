223f260272f0c755622a777bcb33c11ecb137e4b
Rahul Roshan Kachchap <rahulroshan.kachchap@windriver.com>
2024-12-09 00:15:34 -0500

    Switch from Directory Check to Mountpoint Validation
    The original script checked the existence of the ipsec directory under
    /var/luks/stx/luks_fs to determine readiness, due to which ipsec service
    started to load certificate BEFORE luks-fs-mgr declare LUKS is opened
    and Encrypted Vault is mounted, during booting.
    
    The updated script now uses mountpoint -q to verify whether the LUKS
    filesystem is mounted at /var/luks/stx/luks_fs. The new implementation
    explicitly validates the actual state of the mount point, which is a
    more accurate check for the readiness of the LUKS filesystem.
    
    Test Plan:
    PASS: Deploy a multi-nodes system, check daemon.log, verify that
          IPsec service is waiting for the luks volumn to be ready,
          and once it's ready, IPsec service will be started and
          loads cert/keys from the luks directory.
    
    Closes-Bug: 2091261
    
    Change-Id: Idbca297af4b6579179f2659453a5182b2a3df069
    Signed-off-by: Rahul Roshan Kachchap <rahulroshan.kachchap@windriver.com>

--- sysinv/sysinv/sysinv/scripts/check-ipsec-luks-dir.sh
+++ sysinv/sysinv/sysinv/scripts/check-ipsec-luks-dir.sh
@@ -1,14 +1,28 @@
 #!/bin/bash
 
 FIRST_BOOT="/etc/platform/.first_boot"
-LUKS_DIR="/var/luks/stx/luks_fs/ipsec"
+LUKS_MOUNT="/var/luks/stx/luks_fs"
+SLEEP_INTERVAL=1
 
-if [ -e ${FIRST_BOOT} ]; then
+# Exit if the system is in the first boot state
+if [ -e "${FIRST_BOOT}" ]; then
+    echo "First boot detected. Exiting script."
     exit 0
 fi
 
-test -d $LUKS_DIR
-while [ $? != 0 ]; do
-    sleep 1
-    test -d $LUKS_DIR
+# Check if the mount point is already mounted
+if /usr/bin/mountpoint -q "$LUKS_MOUNT"; then
+    echo "Mount point is already mounted: $LUKS_MOUNT"
+    exit 0
+else
+    echo "Mount point is not mounted: $LUKS_MOUNT"
+fi
+
+# Loop to wait for the mount point
+while ! /usr/bin/mountpoint -q "$LUKS_MOUNT"; do
+    echo "Waiting for mount point to be mounted: $LUKS_MOUNT"
+    sleep $SLEEP_INTERVAL
 done
+
+echo "Mount point is now mounted: $LUKS_MOUNT"
+exit 0
