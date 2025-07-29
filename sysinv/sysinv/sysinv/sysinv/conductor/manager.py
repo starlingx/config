96151025eb9916e2d1e5abd5c50d6d8a19245beb
Joaci Morais <Joaci.deMorais@windriver.com>
2025-07-29 11:45:41 -0300

    Reload cert-alarm when enable dual-stack
    Cert-alarm raises alarm for platform certificates when changing
    system from single stack to dual stack. In that case cert-alarm need
    synchronize with the updated platform certificates after dual-stack.
    
    Test Plan:
    PASS: Build ISO with the changes.
    PASS: Deploy a SX.
    PASS: Make sure the duration of your platform certificates are
          1h and renewBefore is 5m.
    PASS: Change to dual-stack by adding a new oam interface by following
          the official guide 'IPv4/IPv6 Dual-Stack Network'.
    PASS: Check in sysinv.log if the runtime manifest is applied
          successfully.
    PASS: Check in sm-customer.log if cert-alarm has been reloaded.
    PASS: Check if the cert-alarm service has been successfully reloaded.
    PASS: Check if there is no certificate alarms for the platform
          certificates
    
    Closes-Bug: 2119298
    Depends-On: https://review.opendev.org/c/starlingx/stx-puppet/+/956084
    
    Change-Id: I9611f907f0c8361b40a7147afe7604ef3ec37c3e
    Signed-off-by: Joaci Morais <Joaci.deMorais@windriver.com>

--- sysinv/sysinv/sysinv/sysinv/conductor/manager.py
+++ sysinv/sysinv/sysinv/sysinv/conductor/manager.py
@@ -9866,7 +9866,8 @@ class ConductorManager(service.PeriodicService):
                             'platform::haproxy::runtime',
                             'platform::ntp::server',
                             'platform::dockerdistribution::config',
-                            'platform::dockerdistribution::runtime'],
+                            'platform::dockerdistribution::runtime',
+                            'platform::certalarm::reload'],
                 puppet_common.REPORT_STATUS_CFG:
                     puppet_common.REPORT_OPENSTACK_ENDPOINTS_CONFIG_REQUESTED
             }
