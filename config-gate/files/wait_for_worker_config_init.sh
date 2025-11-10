cef552a828a1897a7b5910e9e2cc0ef11dcf0874
Joshua Kraitberg <joshua.kraitberg@windriver.com>
2025-11-10 11:09:24 -0500

    Improve error state handling in wait_for_config_init
    Not all states were correctly handled.  The scripts were returning RC=0
    even if the config failed.
    
    TEST PLAN
    PASS: AIO-SX deploy
    PASS: Standard+Storage deploy
    PASS: AIO-SX with failed confg
    * wait_for_config.service propagates error
    * No change to other behaviours
    
    Story: 2011590
    Task: 53117
    Change-Id: Ic4dc047f71aaf545a2309bb3143c1c20844444d8
    Signed-off-by: Joshua Kraitberg <joshua.kraitberg@windriver.com>

--- config-gate/files/wait_for_worker_config_init.sh
+++ config-gate/files/wait_for_worker_config_init.sh
@@ -10,9 +10,16 @@
 SERVICE=workerconfig.service
 
 while :; do
-    systemctl status $SERVICE |grep -q running
-    if [ $? -ne 0 ]; then
+    read -r RESULT ACTIVE_STATE SUB_STATE < <(
+        systemctl show "$SERVICE" -p ActiveState -p SubState -p Result --value |
+            paste - - -
+    )
+    if [[ "$ACTIVE_STATE" == "inactive" && "$RESULT" == "success" && "$SUB_STATE" == "dead" ]]; then
         exit 0
+    elif [[ "$ACTIVE_STATE" == "active" && "$RESULT" == "success" && "$SUB_STATE" == "exited" ]]; then
+        exit 0
+    elif [[ "$ACTIVE_STATE" == "failed" || "$RESULT" == "failed" ]]; then
+        exit 1
     fi
     sleep 1
 done
