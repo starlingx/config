11ccf10f3980eee1d3a2e01ef0115e7cd1c15651
Heitor Matsui <heitorvieira.matsui@windriver.com>
2026-09-15 17:48:28 -0300

    Fix pre-upgrade-deploy incorrect load alarm logic
    The pre-upgrade-deploy is a special type of deployment that
    has the n+1 or n+2 version but is installed in (and thus removed
    from) the from-release feed. The current logic works fine for
    the forward path (applying pre-upgrade-deploy), but for the
    removal scenario it considers the host should be running the
    n+1 release and creates a false incorrect load alarm.
    
    This commit fixes the issue by treating the special scenario
    of pre-upgrade-deploy correctly.
    
    Test Plan
    PASS: apply pre-upgrade-deploy and verify no alarm
    PASS: remove pre-upgrade-deploy and verify no alarm
    (Regression)
    PASS: apply a patch and verify no alarm
    PASS: remove a patch and verify no alarm
    PASS: lock/unlock a host without active deployment
          and verify no alarm
    
    Closes-bug: 2167393
    
    Change-Id: I783f8c94c93f3814d028159315fc72d64fd253c8
    Signed-off-by: Heitor Matsui <heitorvieira.matsui@windriver.com>

--- sysinv/sysinv/sysinv/sysinv/conductor/manager.py
+++ sysinv/sysinv/sysinv/sysinv/conductor/manager.py
@@ -16485,14 +16485,17 @@ class ConductorManager(service.PeriodicService):
         host_upgrade = usm_service.UsmHostUpgrade.get_by_hostname(self.dbapi,
                                                                   host.hostname)
         if host_upgrade:
-            if (host_upgrade.state in constants.DEPLOY_HOST_DEPLOYED_STATES
-                    and not host_upgrade.pre_upgrade_deploy):
-                # if USM upgrade in progress and host is deployed target_load is the to_release
-                target_sw_version = host_upgrade.to_sw_version
+            if host_upgrade.state in constants.DEPLOY_HOST_DEPLOYED_STATES:
+                # For pre-upgrade-deploy type of deployment, the target release is always the
+                # lowest, as pre-upgrade-deploy is deployed (and removed) in the from_release feed
+                if host_upgrade.pre_upgrade_deploy:
+                    target_sw_version = min(host_upgrade.from_sw_version, host_upgrade.to_sw_version,
+                                            key=lambda r: tuple(int(x) for x in r.split(".")))
+                # If USM upgrade in progress and host is deployed target_load is the to_release
+                else:
+                    target_sw_version = host_upgrade.to_sw_version
             else:
-                # otherwise target_load is from_release
-                # pre-upgrade-deploy is installed into the from_release, so the host
-                # is expected to keep running from_release even when deployed
+                # Otherwise, target_load should be from_release
                 target_sw_version = host_upgrade.from_sw_version
             LOG.info(f"USM {'pre-upgrade-deploy ' if host_upgrade.pre_upgrade_deploy else ''}"
                      "deployment in progress, host_state=%s, %s should be running %s" % (
